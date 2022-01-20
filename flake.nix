534b1ac4ab083432a4f629d64f20e7d34c4b0454
Michael Bock <me@thought2.de>
2022-01-20 11:44:00 +0100

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  # inspired by: https://serokell.io/blog/practical-nix-flakes#packaging-existing-applications
+  description = "Circles Pink monorepo";
+  inputs.nixpkgs.url = "nixpkgs";
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      });
+    in
+    {
+      overlay = (final: prev: { });
+      packages = forAllSystems (system: { });
+      defaultPackage = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system}; in
+        pkgs.writeText "hello.txt" "hello circles pink!");
+      #checks = self.packages;
+      devShell = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in
+        pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            hello
+          ];
+
+          # Change the prompt to show that you are in a devShell
+          shellHook = "export PS1='\\e[1;34mdev > \\e[0m'";
+        });
+    };
+}
