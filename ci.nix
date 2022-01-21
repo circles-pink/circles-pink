874ac7259f1564e14444a7d0de58651bd429f2c1
Michael Bock <mb@thought2.de>
2022-01-21 17:05:55 +0100

    Add deployment

--- ci.nix
+++ ci.nix
@@ -1,9 +1,25 @@
 let
   flake = import ./default.nix;
   pkgs = flake.packages.x86_64-linux.pkgs;
+
+
+  # update hash if desired or use different pinning solution
+  effectsSrc = builtins.fetchTarball
+    "https://github.com/hercules-ci/hercules-ci-effects/archive/b67cfbbb31802389e1fb6a9c75360968d201693b.tar.gz";
+
+  inherit (pkgs.effects) runNixOps runIf;
+  inherit (pkgs) lib;
 in
 {
   x86_64-linux = pkgs.recurseIntoAttrs {
     hello = flake.defaultPackage.x86_64-linux;
   };
+
+  neat-network = runIf true (
+    runNixOps {
+      name = "foo";
+      src = lib.cleanSource ./.;
+      networkFiles = [ "networks/network.nix" ];
+    }
+  );
 }
