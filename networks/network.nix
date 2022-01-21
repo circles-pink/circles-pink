b412e2697436d11c0148db8b0083d636a9861feb
Michael Bock <mb@thought2.de>
2022-01-21 17:09:50 +0100

    Fix ci

--- networks/network.nix
+++ networks/network.nix
@@ -1,6 +1,6 @@
 let
   key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCpd/8INly7Mnj+QyRme+M98o+J+hrYOUJiB4znDtptl2qgjN4FJzicJUEOMbVwtan8fsFtuRhmsoQnfxLaX1L7EOSHMmbvobQkvdzHW8nKx3oK49zkmLXQ2VDRZ3D10/x1fXN+1r7PPeKn/2BuGrOjFRAuA4Nbg2S0hHQNAoUhsoI+FbFTYxo/r74BbA/ppodhaMfVU+rOSIY3/9PoHcMT6ofW4F2CdG/iO2GM1zb5Afm8spxgyrXjzUD0kMXj8Y7V65eq8R/9K3Ql9c28sZamKQBYAJOpYzbxc4g5b7YH+Ga9IOTD6OSUAZpsSuj6NZMtZGiPKj17WlduaOo1OOHcKJD+ReHlPAjK3JbU46PmQ173jk7CqNTBw+EiVjOsQRMBbmeUBMU7ERTV5k40V3titLvFIAO9DAD7c7JtWZal6fOH9CPTWuxAA0E4MW8MORJqjMQZ39kCwzcM7NybAGeStinhMp+KhDZNfzodxhoxmG4aEFmiXMvIB+Uh86U6gUs= circles-pink";
-  teal = import ../default.nix;
+  # teal = import ../default.nix;
 in
 {
   network.description = "Circles Pink Network";
@@ -14,7 +14,10 @@ in
       services.httpd.virtualHosts = {
         "circles.pink" = {
           listen = [{ port = 80; }];
-          documentRoot = teal.entries;
+          documentRoot = pkgs.runCommand "ff" { } ''
+            mkdir $out
+            echo bla > $out/index.html
+          '';
         };
       };
 
