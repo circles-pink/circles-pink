b0fefd2428e3b343ccb8ce7a667a04faac89eeb7
nmauersberg <n.mauersberg@gmail.com>
2022-02-03 12:37:41 +0100

    Fix storybook

--- pkgs/ts/.storybook/main.js
+++ pkgs/ts/.storybook/main.js
@@ -1,5 +1,13 @@
 module.exports = {
-
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
   "stories": [
     "../src/stories/**/*.stories.mdx",
     "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
--- pkgs/ts/src/stories/Demo.stories.tsx
+++ pkgs/ts/src/stories/Demo.stories.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-
 import * as StateOnboardTS from "../../../../generated/output/Core.State.Onboard.TS"
-
 import * as StateOnboard from "../../../../generated/output/Core.State.Onboard"
 
 
-// TODO: remove!!
-
-const x = StateOnboardTS.reducerPromise({
+const x = () => StateOnboardTS.reducerPromise({
+    apiCheckUserName: (u: string) => 1 as unknown as Promise<boolean>,
     apiCheckEmail: (e: string) => 1 as unknown as Promise<boolean>,
-    apiCheckUserName: (u: string) => 1 as unknown as Promise<boolean>
 })(StateOnboard.SetUsername.create("hello"))(StateOnboard.init)
 
 import { ComponentMeta } from '@storybook/react';
