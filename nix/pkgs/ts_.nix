f969b314bc4170202933cdd1ceeb66e45d85071c
Michael Bock <mb@thought2.de>
2022-02-04 13:58:44 +0100

    Finish poc

--- nix/pkgs/ts_.nix
+++ nix/pkgs/ts_.nix
@@ -1,9 +1,48 @@
 { pkgs, ... }: rec {
-  foo = ../../.;
+  src = pkgs.nix-filter.filter {
+    root = ../../.;
+    include = [
+      "package.json"
+      "yarn.lock"
+      "pkgs/ts_/cli/package.json"
+      "pkgs/ts_/storybook/package.json"
+      "pkgs/ts_/common/package.json"
+    ];
+  };
 
+  localPackages = {
+    common = ../../pkgs/ts_/common;
+    storybook = ../../pkgs/ts_/storybook;
+    cli = ../../pkgs/ts_/cli;
+  };
 
-  #   workspaces = pkgs.yarn2nix-moretea.mkYarnWorkspace {
-  #     src = ../../.;
-  #   };
+  emptyWorkspaces = pkgs.yarn2nix-moretea.mkYarnWorkspace {
+    inherit src;
+  };
+
+  replaceEmptyLocalDep = pkgName: pkgs.lib.pipe localPackages [
+    (pkgs.lib.mapAttrsToList (depName: drv: ''
+      dir="$tmp/libexec/${pkgName}/deps/${depName}"
+      if [ -d $dir ];
+      then
+        rm -rf $dir
+        cp -r ${drv} $dir
+      fi
+    ''))
+    (builtins.concatStringsSep "\n")
+  ];
+
+  workspaces =
+    builtins.mapAttrs
+      (name: value: pkgs.runCommand "" { } ''
+        tmp=`mktemp -d`
+        cp -r ${value}/* -t $tmp
+        chmod -R +w $tmp
+
+        ${replaceEmptyLocalDep name}
+
+        cp -r $tmp $out
+      '')
+      emptyWorkspaces;
 
 }
