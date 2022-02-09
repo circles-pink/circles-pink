5efb1eccb49aa612404773810a0776fd59d2863f
Michael Bock <mb@thought2.de>
2022-02-09 13:19:22 +0100

    Fix API

--- pkgs/purs/src/Garden/Env.purs
+++ pkgs/purs/src/Garden/Env.purs
@@ -3,59 +3,34 @@ module Garden.Env
   ) where
 
 import Prelude
-import Affjax (printError)
-import Affjax as AX
-import Affjax.RequestBody (RequestBody(..))
-import Affjax.ResponseFormat as ResponseFormat
 import Core.State.Onboard as O
-import Data.Argonaut (class DecodeJson, class EncodeJson, decodeJson, encodeJson)
-import Data.Argonaut.Core (fromObject, fromString, stringifyWithIndent)
-import Data.Either (Either(..))
+import Data.Argonaut (encodeJson)
 import Data.HTTP.Method (Method(..))
-import Data.Maybe (Maybe(..))
-import Data.Tuple.Nested ((/\))
 import Effect.Aff (Aff)
-import Effect.Class.Console (log)
-import Foreign.Object (fromFoldable)
 import HTTP (ReqFn)
-import Undefined (undefined)
 
 env :: { request :: ReqFn } -> O.Env Aff
 env { request } =
   { apiCheckUserName:
-      \user -> do
-        log $ "BBBchecking " <> user
+      \username ->
         request
           { url: "https://api.circles.garden/api/users"
           , method: POST
-          , body: encodeJson { username: user }
+          , body: encodeJson { username }
           }
-          <#> ( \{ status, body } -> case status of
-                0 -> true
+          <#> ( \{ status } -> case status of
+                200 -> true
                 _ -> false
             )
   , apiCheckEmail:
-      \email -> do
-        log $ "checking " <> email
-        result <-
-          AX.request
-            $ AX.defaultRequest
-                { url = "https://api.circles.garden/api/users"
-                , method = Left POST
-                , responseFormat = ResponseFormat.json
-                , content =
-                  Just
-                    $ Json
-                    $ fromObject
-                    $ fromFoldable [ "email" /\ fromString email ]
-                }
-        log ("API CALL EMAIL: " <> email)
-        case result of
-          Left err -> do
-            log "ERROR"
-            log $ printError err
-            pure false
-          Right { body } -> do
-            log $ stringifyWithIndent 2 body
-            pure true
+      \email ->
+        request
+          { url: "https://api.circles.garden/api/users"
+          , method: POST
+          , body: encodeJson { email }
+          }
+          <#> ( \{ status } -> case status of
+                200 -> true
+                _ -> false
+            )
   }
