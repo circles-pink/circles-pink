bbeb421bcc1075ed7f3a7e3b5c1f51f754b55bf9
Michael Bock <mb@thought2.de>
2022-02-09 18:13:26 +0100

    WIP

--- pkgs/ts/storybook/.storybook/preview.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { addDecorator } from '@storybook/react';
-// import { withInfo } from '@storybook/addon-info';
-import Layout from './Layout';
-addDecorator(storyFn => <Layout>{storyFn()}</Layout>);
-// addDecorator(withInfo({
-//   inline: true,
-//   propTablesExclude: [Layout]
-// }));
