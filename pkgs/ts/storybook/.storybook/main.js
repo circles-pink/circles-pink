748ea3daa1e04083511b62ce6823cad8fae6305b
nmauersberg <n.mauersberg@gmail.com>
2022-02-09 20:37:37 +0100

    Fix react render

--- pkgs/ts/storybook/.storybook/main.js
+++ pkgs/ts/storybook/.storybook/main.js
@@ -1,11 +1,30 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
-  "framework": "@storybook/react"
+  framework: "@storybook/react",
+  webpackFinal: async config => {
+    config.module.rules = [
+      ...config.module.rules,
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: require.resolve("babel-loader"),
+            options: {
+              presets: [require.resolve("babel-preset-react-app")]
+            }
+          },
+          require.resolve("react-docgen-typescript-loader")
+        ]
+      },
+    ];
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
 }
