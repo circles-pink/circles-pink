9af74900adb663d8dd101e7de3b73ff5b5b050b8
nmauersberg <n.mauersberg@gmail.com>
2022-02-18 14:00:54 +0100

    Add basic Statemachine

--- pkgs/purs/src/CirclesPink/StateMachine.purs
+++ pkgs/purs/src/CirclesPink/StateMachine.purs
@@ -1,14 +1,20 @@
 module CirclesPink.StateMachine
   ( CirclesStateMachine
+  , check
   ) where
 
+import Prelude
 import CirclesPink.StateMachine.Action as A
 import CirclesPink.StateMachine.Protocol as P
 import CirclesPink.StateMachine.State as S
 import Stadium.Type.StateMachine as STM
+import Type.Proxy (Proxy(..))
 
 type CirclesStateMachine
   = STM.StateMachine
       P.CirclesProtocol
       S.CirclesState
       A.CirclesAction
+
+check :: Unit
+check = STM.validate (Proxy :: _ CirclesStateMachine)
--- pkgs/purs/src/CirclesPink/StateMachine/State.purs
+++ pkgs/purs/src/CirclesPink/StateMachine/State.purs
@@ -1,12 +1,43 @@
 module CirclesPink.StateMachine.State
   ( CirclesState
+  , UserData
+  , UsernameCheckResult
+  , _askUsername
+  , _infoGeneral
+  , init
   ) where
 
-import Prelude
-import Data.Variant (Variant)
+import Data.Variant (Variant, inj)
+import Type.Proxy (Proxy(..))
+
+type UsernameCheckResult
+  = { username :: String
+    , isValid :: Boolean
+    }
+
+type UserData
+  = { username :: String
+    , usernameCheckResult :: UsernameCheckResult
+    }
 
 type CirclesState
   = Variant
-      ( state1 :: Int
-      , state2 :: String
+      ( infoGeneral :: UserData
+      , askUsername :: UserData
       )
+
+init :: CirclesState
+init =
+  _infoGeneral
+    { username: ""
+    , usernameCheckResult:
+        { username: ""
+        , isValid: false
+        }
+    }
+
+_infoGeneral :: forall a v. a -> Variant ( infoGeneral :: a | v )
+_infoGeneral = inj (Proxy :: _ "infoGeneral")
+
+_askUsername :: forall a v. a -> Variant ( askUsername :: a | v )
+_askUsername = inj (Proxy :: _ "askUsername")
