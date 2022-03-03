a0defe676aef296eb6c735465074d47ee828968f
nmauersberg <n.mauersberg@gmail.com>
2022-03-03 12:24:32 +0100

    FadeIn animation with direction and delay

--- pkgs/ts/circles/src/onboarding/view/InfoGeneral.tsx
+++ pkgs/ts/circles/src/onboarding/view/InfoGeneral.tsx
@@ -1,27 +1,42 @@
 import * as A from 'generated/output/CirclesPink.Garden.StateMachine.Action';
 import { unit } from 'generated/output/Data.Unit';
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import { ButtonPink } from '../../components/forms';
 import { Claim, SubClaim, Text } from '../../components/text';
 import { DialogCard } from '../DialogCard';
+import { Direction, FadeIn } from '../../components/animation/FadeIn';
 
 type InfoGeneralProps = {
   act: (ac: A.CirclesAction) => void;
 };
 
 export const InfoGeneral = ({ act }: InfoGeneralProps): ReactElement => {
+  const direction: Direction = 'left';
+  const incrementBy = 0.1;
+  const getDelay = (
+    n => () =>
+      (n += incrementBy)
+  )(0 - incrementBy);
+
   return (
     <DialogCard
       text={
         <Text>
-          <Claim>Welcome to Circles</Claim>
-          <SubClaim>Let's get you a circles Wallet!</SubClaim>
+          <FadeIn direction={direction} delay={getDelay()}>
+            <Claim>Welcome to Circles</Claim>
+          </FadeIn>
+
+          <FadeIn direction={direction} delay={getDelay()}>
+            <SubClaim>Let's get you a circles Wallet!</SubClaim>
+          </FadeIn>
         </Text>
       }
       control={
-        <ButtonPink onClick={() => act(A._infoGeneral(A._next(unit)))}>
-          Next
-        </ButtonPink>
+        <FadeIn direction={direction} delay={getDelay()}>
+          <ButtonPink onClick={() => act(A._infoGeneral(A._next(unit)))}>
+            Next
+          </ButtonPink>
+        </FadeIn>
       }
     />
   );
