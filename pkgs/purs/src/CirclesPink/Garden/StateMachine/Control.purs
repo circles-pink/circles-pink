4c7fa32c9270bf547ee8be7520afc4f33c83c105
Michael Bock <me@thought2.de>
2022-03-03 09:54:10 +0100

    Cleanup

--- pkgs/purs/src/CirclesPink/Garden/StateMachine/Control.purs
+++ pkgs/purs/src/CirclesPink/Garden/StateMachine/Control.purs
@@ -9,15 +9,12 @@ import CirclesPink.Garden.StateMachine.Action (CirclesAction)
 import CirclesPink.Garden.StateMachine.Error (CirclesError)
 import CirclesPink.Garden.StateMachine.State (CirclesState)
 import CirclesPink.Garden.StateMachine.State as S
-import Control.Monad.Except (class MonadTrans, ExceptT(..), lift, runExceptT)
-import Control.Monad.Except.Checked (ExceptV)
+import Control.Monad.Except (class MonadTrans, ExceptT, lift, runExceptT)
 import Data.Either (Either(..))
-import Data.Variant (Variant, default, onMatch)
+import Data.Variant (default, onMatch)
 import Effect.Class (class MonadEffect)
 import RemoteData (RemoteData, _failure, _loading, _success)
 import Stadium.Control as C
-import Type.Row (type (+))
-import Undefined (undefined)
 import Wallet.PrivateKey (PrivateKey)
 
 type Env m
@@ -29,9 +26,6 @@ type Env m
         ExceptT CirclesError m { isValid :: Boolean }
     , generatePrivateKey :: m PrivateKey
     }
-
-mapEnv :: forall m n. Env m -> Env n
-mapEnv = undefined
 
 circlesControl ::
   forall t m.
@@ -119,82 +113,3 @@ circlesControl env =
     , magicWords:
         { prev: \set _ _ -> set $ \st -> S._infoSecurity st }
     }
-
--- { infoGeneral:
---     { next: \set _ _ -> set $ \st -> S._askUsername st }
--- , askUsername:
---     { prev: \set _ _ -> set $ \st -> S._infoGeneral st
---     , setUsername:
---         \set _ username -> do
---           set $ \st -> S._askUsername st { username = username }
---           set
---             $ \st ->
---                 S._askUsername st { usernameApiResult = _loading :: RemoteData CirclesError { isValid :: Boolean } }
---           result <- runExceptT $ env.apiCheckUserName username
---           set
---             $ \st ->
---                 if username == st.username then case result of
---                   Left e -> S._askUsername st { usernameApiResult = _failure e }
---                   Right x -> S._askUsername st { usernameApiResult = _success x }
---                 else
---                   S._askUsername st
---           pure unit
---     , next:
---         \set _ _ ->
---           set
---             $ \st ->
---                 let
---                   usernameValid =
---                     default false
---                       # onMatch
---                           { success: (\r -> r.isValid) }
---                 in
---                   if usernameValid st.usernameApiResult then
---                     S._askEmail st
---                   else
---                     S._askUsername st
---     }
--- , askEmail:
---     { prev: \set _ _ -> set $ \st -> S._askUsername st
---     , setEmail:
---         \set _ email -> do
---           set $ \st -> S._askEmail st { email = email }
---           set
---             $ \st ->
---                 S._askEmail st { emailApiResult = _loading :: RemoteData CirclesError { isValid :: Boolean } }
---           result <- runExceptT $ env.apiCheckEmail email
---           set
---             $ \st ->
---                 if email == st.email then case result of
---                   Left e -> S._askEmail st { emailApiResult = _failure e }
---                   Right x -> S._askEmail st { emailApiResult = _success x }
---                 else
---                   S._askEmail st
---           pure unit
---     , setTerms: \set _ _ -> set $ \st -> S._askEmail st { terms = not st.terms }
---     , setPrivacy: \set _ _ -> set $ \st -> S._askEmail st { privacy = not st.privacy }
---     , next:
---         \set _ _ ->
---           set
---             $ \st ->
---                 let
---                   emailValid =
---                     default false
---                       # onMatch
---                           { success: (\r -> r.isValid) }
---                 in
---                   if (emailValid st.emailApiResult) && st.terms && st.privacy then
---                     S._infoSecurity st
---                   else
---                     S._askEmail st
---     }
--- , infoSecurity:
---     { prev: \set _ _ -> set $ \st -> S._askEmail st
---     , next:
---         \set _ _ -> do
---           pk <- env.generatePrivateKey
---           set $ \st -> S._magicWords st { privateKey = pk }
---     }
--- , magicWords:
---     { prev: \set _ _ -> set $ \st -> S._infoSecurity st }
--- }
