f4818f98a0401d3d702d591b83fae920eaad88fb
Michael Bock <me@thought2.de>
2022-03-09 13:07:46 +0100

    Fix CI

--- flake.nix
+++ flake.nix
@@ -19,90 +19,171 @@
     flake = false;
   };
 
+  inputs.hercules-ci-effects.url = "github:hercules-ci/hercules-ci-effects";
+
+  inputs.flake-compat-ci.url = "github:hercules-ci/flake-compat-ci";
+
   outputs = inputs:
-    inputs.flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let
-        overlay = import ./nix/overlay.nix;
+    let
+      devSystems = [ "x86_64-linux" "aarch64-darwin" ];
+      ciSystems = devSystems;
+      cdSystem = "x86_64-linux";
+      self = inputs.self;
 
-        self = inputs.self;
-        pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [
-            overlay
-            (prev: final: {
-              purescript-tsd-gen = purescript-tsd-gen.defaultPackage.${system};
-              spago2nix = easy-purescript-nix.spago2nix;
-              nix-filter = nix-filter.lib;
-              nodejs = final.nodejs-17_x;
-            })
-          ];
-        };
-        nix-filter = inputs.nix-filter;
-        purescript-tsd-gen = inputs.purescript-tsd-gen;
-        easy-purescript-nix = import inputs.easy-purescript-nix { inherit pkgs; };
-      in
-      {
+      system = "x86_64-linux";
 
-        packages =
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [
+          overlay
+          (prev: final: {
+            purescript-tsd-gen = purescript-tsd-gen.defaultPackage.${system};
+            spago2nix = easy-purescript-nix.spago2nix;
+            nix-filter = nix-filter.lib;
+            nodejs = final.nodejs-17_x;
+          })
+        ];
+      };
+      nix-filter = inputs.nix-filter;
+      purescript-tsd-gen = inputs.purescript-tsd-gen;
+      easy-purescript-nix = import inputs.easy-purescript-nix { inherit pkgs; };
+      overlay = import ./nix/overlay.nix;
+    in
+    (inputs.flake-utils.lib.eachSystem [ "x86_64-linux" ]
+      (system:
+        {
+
+          packages = pkgs.circles-pink;
+
+          checks = self.packages;
+
+          devShell =
+            pkgs.mkShell {
+              nativeBuildInputs = [
+                pkgs.nixpkgs-fmt
+                pkgs.git
+                pkgs.vscode
+                pkgs.bashInteractive
+                pkgs.yarn
+                pkgs.nix-tree
+                pkgs.miniserve
+                pkgs.nodePackages.node2nix
+                pkgs.nodePackages.purty
+                pkgs.nodePackages.typescript
+                pkgs.nodejs-17_x
+                pkgs.purescript
+                pkgs.spago
+                pkgs.purescript-tsd-gen
+                pkgs.spago2nix
+                pkgs.cspell
+                pkgs.ts-node
+                pkgs.circles-pink.patchTsTypes
+                pkgs.gnumake
+                pkgs.fff
+                pkgs.nodePackages.prettier
+                pkgs.dhall
+                pkgs.dhall-lsp-server
+                pkgs.graphviz
+                pkgs.makefile2graph
+                pkgs.depcruise
+              ];
+
+              # Change the prompt to show that you are in a devShell
+              shellHook = ''
+                . ${pkgs.complete-alias}/bin/complete_alias
+              
+                REPO_ROOT=$PWD
+                export EDITOR=codium
+
+                alias code=codium
+                alias mk=make
+                alias cd-root="cd $REPO_ROOT"
+              
+                complete -F _complete_alias mk
+                complete -F _complete_alias code
+
+              '' +
+              "export PS1='\\e[1;36m@circles.pink\\e[0m:\\e[1;34m`echo $PWD | sed s#'$REPO_ROOT'#*#`\\e[0m$ '";
+            };
+        }))
+
+    //
+
+    {
+
+      checks = {
+        ${cdSystem} =
+          let
+            effects = self.effects { src.ref = null; };
+          in
           {
-            hello = pkgs.writeText "hello.txt" "THIS!!!";
-            sample = pkgs.circles-pink.yarn;
-
-            foo = pkgs.purescript-tsd-gen;
-
-            inherit pkgs;
-          } // pkgs.circles-pink
-        ;
-
-        checks = self.packages;
-
-        devShell =
-          pkgs.mkShell {
-            nativeBuildInputs = [
-              pkgs.nixpkgs-fmt
-              pkgs.git
-              pkgs.vscode
-              pkgs.bashInteractive
-              pkgs.yarn
-              pkgs.nix-tree
-              pkgs.miniserve
-              pkgs.nodePackages.node2nix
-              pkgs.nodePackages.purty
-              pkgs.nodePackages.typescript
-              pkgs.nodejs-17_x
-              pkgs.purescript
-              pkgs.spago
-              pkgs.purescript-tsd-gen
-              pkgs.spago2nix
-              pkgs.cspell
-              pkgs.ts-node
-              pkgs.circles-pink.patchTsTypes
-              pkgs.gnumake
-              pkgs.fff
-              pkgs.nodePackages.prettier
-              pkgs.dhall
-              pkgs.dhall-lsp-server
-              pkgs.graphviz
-              pkgs.makefile2graph
-              pkgs.depcruise
-            ];
-
-            # Change the prompt to show that you are in a devShell
-            shellHook = ''
-              . ${pkgs.complete-alias}/bin/complete_alias
-              
-              REPO_ROOT=$PWD
-              export EDITOR=codium
-
-              alias code=codium
-              alias mk=make
-              alias cd-root="cd $REPO_ROOT"
-              
-              complete -F _complete_alias mk
-              complete -F _complete_alias code
-
-            '' +
-            "export PS1='\\e[1;36m@circles.pink\\e[0m:\\e[1;34m`echo $PWD | sed s#'$REPO_ROOT'#*#`\\e[0m$ '";
+            deploy-nixops-example-prebuilt = effects.nixops-example.prebuilt;
+            #deploy-nixops-example-dependencies = effects.nixops-example.dependencies;
           };
-      });
+      };
+
+      ciNix = args@{ src }: inputs.flake-compat-ci.lib.recurseIntoFlakeWith {
+        flake = self;
+        systems = ciSystems;
+        effectsArgs = args;
+      };
+
+
+
+      nixopsConfigurations.default =
+        let
+          accessKeyId = "nixops-example";
+          region = "us-east-1";
+          tags = { };
+          name = "default";
+        in
+        {
+          nixpkgs = inputs.nixpkgs;
+          network.description = name;
+
+          network = {
+            storage.memory = { };
+          };
+
+          # network.storage.hercules-ci = {
+          #   stateName = "${name}.nixops";
+          # };
+
+
+          # network.lock.hercules-ci = {
+          #   stateName = "${name}.nixops";
+          # };
+
+        } // (import ./networks/network.nix { inherit pkgs; });
+
+      effects = { src }:
+        let
+          pkgs = inputs.nixpkgs.legacyPackages.${cdSystem};
+          effects = inputs.hercules-ci-effects.lib.withPkgs pkgs;
+        in
+        {
+
+          # https://docs.hercules-ci.com/hercules-ci-effects/reference/nix-functions/runif/
+          nixops-example = effects.runIf true #(src.ref == "refs/heads/main") (
+
+            # https://docs.hercules-ci.com/hercules-ci-effects/reference/nix-functions/runnixops2/
+            (effects.runNixOps2
+              {
+                flake = self;
+
+                userSetupScript = ''
+                  writeSSHKey
+                  ssh -o "StrictHostKeyChecking no" root@circles.pink whoami
+                  mkdir -p ~/.config/nix
+                  echo 'experimental-features = nix-command flakes' >>~/.config/nix/nix.conf
+                '';
+
+                secretsMap = {
+                  "ssh" = "default-ssh";
+                };
+                forgetState = true;
+
+              });
+        };
+    };
 }
--- networks/network.nix
+++ networks/network.nix
@@ -1,19 +1,19 @@
+{ pkgs, ... }:
+
 let
   key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCpd/8INly7Mnj+QyRme+M98o+J+hrYOUJiB4znDtptl2qgjN4FJzicJUEOMbVwtan8fsFtuRhmsoQnfxLaX1L7EOSHMmbvobQkvdzHW8nKx3oK49zkmLXQ2VDRZ3D10/x1fXN+1r7PPeKn/2BuGrOjFRAuA4Nbg2S0hHQNAoUhsoI+FbFTYxo/r74BbA/ppodhaMfVU+rOSIY3/9PoHcMT6ofW4F2CdG/iO2GM1zb5Afm8spxgyrXjzUD0kMXj8Y7V65eq8R/9K3Ql9c28sZamKQBYAJOpYzbxc4g5b7YH+Ga9IOTD6OSUAZpsSuj6NZMtZGiPKj17WlduaOo1OOHcKJD+ReHlPAjK3JbU46PmQ173jk7CqNTBw+EiVjOsQRMBbmeUBMU7ERTV5k40V3titLvFIAO9DAD7c7JtWZal6fOH9CPTWuxAA0E4MW8MORJqjMQZ39kCwzcM7NybAGeStinhMp+KhDZNfzodxhoxmG4aEFmiXMvIB+Uh86U6gUs= circles-pink";
   # teal = import ../default.nix;
 in
 {
-  network.description = "Circles Pink Network";
 
   webserver =
-    { config, pkgs, modulesPath, ... }:
     let
       system = "x86_64-linux";
     in
     {
       environment.systemPackages = [ pkgs.busybox ];
 
-      boot.tmpOnTmpfs = true;
+      boot.tmpOnTmpfs = false;
       networking.hostName = "circles-pink";
 
       services.openssh.enable = true;
@@ -24,7 +24,8 @@ in
 
       systemd.mounts = [ ];
 
-      imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+      imports = [ ./qemu-guest.nix ];
+
       boot.loader.grub.device = "/dev/sda";
       boot.initrd.kernelModules = [ "nvme" ];
       fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
@@ -43,7 +44,7 @@ in
             enableACME = true;
             serverAliases = [ "circles.pink" ];
             locations."/" = {
-              root = (import ../default.nix).packages.${system}.publicDir;
+              root = pkgs.circles-pink.publicDir;
             };
           };
         };
