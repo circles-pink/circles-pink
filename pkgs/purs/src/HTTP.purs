464adecb81eed616f0d974c03e6a6565492b7599
Michael Bock <me@thought2.de>
2022-03-10 16:39:20 +0100

    Add logging

--- pkgs/purs/src/HTTP.purs
+++ pkgs/purs/src/HTTP.purs
@@ -4,16 +4,18 @@ module HTTP
   , ReqFn
   , Res
   , _errUnknown
+  , addLogging
   ) where
 
 import Prelude
 import Control.Monad.Except (ExceptT(..))
 import Control.Monad.Except.Checked (ExceptV)
-import Data.Argonaut (Json)
+import Data.Argonaut (Json, stringify)
 import Data.Either (Either)
 import Data.HTTP.Method (Method)
 import Data.Variant (Variant, inj)
 import Effect.Aff (Aff)
+import Effect.Class.Console (log)
 import Type.Proxy (Proxy(..))
 import Type.Row (type (+))
 
@@ -31,3 +33,10 @@ _errUnknown = inj (Proxy :: _ "errNetwork") unit
 
 type ReqFn r
   = Req -> ExceptV (NetworkError + r) Aff Res
+
+addLogging :: forall r. ReqFn r -> ReqFn r
+addLogging reqFn req = do
+  log ("HTTP REQUEST: " <> show req.method <> " " <> show req.url <> " " <> (stringify req.body))
+  res <- reqFn req
+  log ("HTTP RESPONSE: " <> show res.status <> " " <> (stringify req.body))
+  pure res
