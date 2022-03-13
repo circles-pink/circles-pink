864e885c8ad377cfef56734f2a260b52ae77c3f2
Michael Bock <me@thought2.de>
2022-03-13 08:41:53 +0100

    Add Dot Language

---
+++ pkgs/purs/src/Language/Dot/Attr.purs
@@ -0,0 +1,59 @@
+module Language.Dot.Attr
+  ( Attr
+  , E(..)
+  , G(..)
+  , N(..)
+  , S(..)
+  , Shape(..)
+  , box_
+  , class Label
+  , label
+  , labelImpl
+  , shape
+  ) where
+
+import Prelude
+import Data.Variant (Variant, inj)
+import Language.Dot.Id (Id(..))
+import Type.Proxy (Proxy(..))
+import Unsafe.Coerce (unsafeCoerce)
+
+newtype Attr :: forall k. k -> Type
+newtype Attr a
+  = Attr { key :: Id, value :: Id }
+
+data N
+
+data E
+
+data G
+
+data S
+
+class Label :: forall k. k -> Constraint
+class Label a where
+  label :: String -> Attr a
+
+instance labelN :: Label N where
+  label = labelImpl
+
+labelImpl :: forall a. String -> Attr a
+labelImpl value = Attr { key: Id "label", value: Id value }
+
+shape :: Shape -> Attr N
+shape s = Attr { key: Id "label", value: Id $ shapeToString s }
+
+--------------------------------------------------------------------------------
+-- AttrTypes
+--------------------------------------------------------------------------------
+type Shape
+  = Variant
+      ( box :: Unit
+      , ellipse :: Unit
+      )
+
+box_ :: forall v. Variant ( box :: Unit | v )
+box_ = inj (Proxy :: _ "box") unit
+
+shapeToString :: Shape -> String
+shapeToString s = unsafeCoerce (unsafeCoerce s).type
---
+++ pkgs/purs/src/Language/Dot/Graph/Print.purs
@@ -0,0 +1,8 @@
+module Language.Dot.Graph.Print where
+
+import Prelude
+import Language.Dot.Graph (Graph(..))
+import Undefined (undefined)
+
+printGraph :: Graph -> String
+printGraph = undefined
