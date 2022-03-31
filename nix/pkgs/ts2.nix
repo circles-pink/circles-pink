dee36268dbfdcf0ac171c8ad221923a667582547
Michael Bock <mb@thought2.de>
2022-03-31 12:04:56 +0200

    Enhance ts2

--- nix/pkgs/ts2.nix
+++ nix/pkgs/ts2.nix
@@ -5,17 +5,22 @@ let
     tar -xf ${pkgs.fetchurl x} --directory $out
   '';
 in
-{
+rec {
   yarnLockJson = pkgs.runCommand "yarn-lock.json" { buildInputs = [ pkgs.yarnLockToJson ]; }
     ''
       yarn-lock-to-json ${../../yarn.lock} > $out
     '';
 
-  #yarnSources = builtins.mappAttribs (v: fetchTarball {}) yarnLockJson;
+  yarnLock = builtins.fromJSON (builtins.readFile yarnLockJson);
 
-  yarnSource = fetchTarball {
-    name = "a";
-    url = "https://registry.yarnpkg.com/@aws-crypto/ie11-detection/-/ie11-detection-2.0.0.tgz#bb6c2facf8f03457e949dcf0921477397ffa4c6e";
-    sha512 = "sha512-pkVXf/dq6PITJ0jzYZ69VhL8VFOFoPZLZqtU/12SGnzYuJOOGNfF41q9GxdI1yqC8R13Rq3jOLKDFpUJFT5eTA==";
-  };
+  yarnSources = builtins.mapAttrs
+    (n: v: fetchTarball {
+      name = builtins.replaceStrings [ "@" "/" ] [ "__at__" "__slash__" ] n;
+      url = v.resolved;
+      sha512 = v.integrity;
+    })
+    yarnLock;
+
+  x = yarnSources.${''@aws-crypto/sha256-browser@2.0.0''};
+
 }
