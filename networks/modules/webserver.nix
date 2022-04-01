b110a4f0bf928ed7f0ebcddca45968c69123a952
Michael Bock <me@thought2.de>
2022-04-01 11:18:03 +0200

    Add locations to service config

--- networks/modules/webserver.nix
+++ networks/modules/webserver.nix
@@ -2,11 +2,18 @@
 
 let
   key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCpd/8INly7Mnj+QyRme+M98o+J+hrYOUJiB4znDtptl2qgjN4FJzicJUEOMbVwtan8fsFtuRhmsoQnfxLaX1L7EOSHMmbvobQkvdzHW8nKx3oK49zkmLXQ2VDRZ3D10/x1fXN+1r7PPeKn/2BuGrOjFRAuA4Nbg2S0hHQNAoUhsoI+FbFTYxo/r74BbA/ppodhaMfVU+rOSIY3/9PoHcMT6ofW4F2CdG/iO2GM1zb5Afm8spxgyrXjzUD0kMXj8Y7V65eq8R/9K3Ql9c28sZamKQBYAJOpYzbxc4g5b7YH+Ga9IOTD6OSUAZpsSuj6NZMtZGiPKj17WlduaOo1OOHcKJD+ReHlPAjK3JbU46PmQ173jk7CqNTBw+EiVjOsQRMBbmeUBMU7ERTV5k40V3titLvFIAO9DAD7c7JtWZal6fOH9CPTWuxAA0E4MW8MORJqjMQZ39kCwzcM7NybAGeStinhMp+KhDZNfzodxhoxmG4aEFmiXMvIB+Uh86U6gUs= circles-pink";
-  # teal = import ../default.nix;
 
   Makefile = pkgs.writeText "MakeFile" (builtins.readFile ./webserver/Makefile);
 
-  serviceUrls = builtins.mapAttrs (name: value: lib.mkUrl value.url) config.env.services;
+  mkServiceUrl = name: value:
+    let
+      url = lib.mkUrl value.url;
+    in
+    if value.locations == null
+    then url
+    else builtins.mapAttrs (_: r: url + r) value.locations;
+
+  serviceUrls = builtins.mapAttrs mkServiceUrl config.env.services;
 in
 
 {
@@ -28,6 +35,10 @@ in
     };
     directus = {
       url = (config.env.url // { subdomain = "directus"; });
+      locations = {
+        "/graphql" = "/graphql";
+        "/graphql/system" = "/graphql/system";
+      };
       port = 8055;
     };
     mysql = {
