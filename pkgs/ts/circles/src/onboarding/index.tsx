aad72bcd0c4aff5ab7849a9e62034fbba58fb96d
Michael Bock <mb@thought2.de>
2022-04-05 21:03:31 +0200

    Fix env var issue

--- pkgs/ts/circles/src/onboarding/index.tsx
+++ pkgs/ts/circles/src/onboarding/index.tsx
@@ -40,34 +40,35 @@ export const Onboarding = (props: OnboardingProps) => {
   );
 };
 
-const control = mkControl(
-  process.env.STORYBOOK_GARDEN_API !== undefined &&
-    process.env.STORYBOOK_GARDEN_API_USERS !== undefined &&
-    process.env.STORYBOOK_GRAPH_API !== undefined &&
-    process.env.STORYBOOK_GARDEN_SUBGRAPH_NAME !== undefined &&
-    process.env.STORYBOOK_GARDEN_RELAY !== undefined &&
-    process.env.STORYBOOK_GARDEN_HUB_ADDRESS !== undefined &&
-    process.env.STORYBOOK_GARDEN_PROXY_FACTORY_ADRESS !== undefined &&
-    process.env.STORYBOOK_GARDEN_SAFE_MASTER_ADDRESS !== undefined &&
-    process.env.STORYBOOK_GARDEN_ETHEREUM_NODE_WS !== undefined
-    ? {
-        gardenApi: process.env.STORYBOOK_GARDEN_API,
-        gardenApiUsers: process.env.STORYBOOK_GARDEN_API_USERS,
-        gardenGraphApi: process.env.STORYBOOK_GRAPH_API,
-        gardenSubgraphName: process.env.STORYBOOK_GARDEN_SUBGRAPH_NAME,
-        gardenRelay: process.env.STORYBOOK_GARDEN_RELAY,
-        gardenHubAddress: process.env.STORYBOOK_GARDEN_HUB_ADDRESS,
-        gardenProxyFactoryAddress:
-          process.env.STORYBOOK_GARDEN_PROXY_FACTORY_ADRESS,
-        gardenSafeMasterAddress:
-          process.env.STORYBOOK_GARDEN_SAFE_MASTER_ADDRESS,
-        gardenEthereumNodeWebSocket:
-          process.env.STORYBOOK_GARDEN_ETHEREUM_NODE_WS,
-      }
-    : (() => {
-        throw new Error('Env vars missing');
-      })()
-) as never;
+const envRaw = {
+  gardenApi: process.env.STORYBOOK_GARDEN_API,
+  gardenApiUsers: process.env.STORYBOOK_GARDEN_API_USERS,
+  gardenGraphApi: process.env.STORYBOOK_GARDEN_GRAPH_API,
+  gardenSubgraphName: process.env.STORYBOOK_GARDEN_SUBGRAPH_NAME,
+  gardenRelay: process.env.STORYBOOK_GARDEN_RELAY,
+  gardenHubAddress: process.env.STORYBOOK_GARDEN_HUB_ADDRESS,
+  gardenProxyFactoryAddress: process.env.STORYBOOK_GARDEN_PROXY_FACTORY_ADRESS,
+  gardenSafeMasterAddress: process.env.STORYBOOK_GARDEN_SAFE_MASTER_ADDRESS,
+  gardenEthereumNodeWebSocket: process.env.STORYBOOK_GARDEN_ETHEREUM_NODE_WS,
+};
+
+type RemoveUndefined<T> = { [key in keyof T]: Exclude<T[key], undefined> };
+
+type Env = RemoveUndefined<typeof envRaw>;
+
+const parseEnv = (env_: typeof envRaw): Env => {
+  const xs = Object.entries(env_).forEach(([k, v]) => {
+    if (v === undefined) {
+      throw new Error(`Env var missing: ${k}`);
+    }
+  });
+
+  return env_ as Env;
+};
+
+const env = parseEnv(envRaw);
+
+const control = mkControl(env);
 
 const OnboardingContent = ({
   initState,
