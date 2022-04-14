271dc1d164f2bbd1de081b7f7884a09ccb625b8c
Michael Bock <mb@thought2.de>
2022-04-14 18:57:54 +0200

    Start TrustGraph UI

---
+++ pkgs/ts/storybook/src/demo/TrustGraph.stories.tsx
@@ -0,0 +1,24 @@
+import React, { ReactElement } from "react";
+import { ComponentMeta } from "@storybook/react";
+import { TrustGraph, Graph } from "circles/src/components/TrustGraph";
+import { TrustNode } from "generated/output/CirclesCore";
+import { unsafeAddrFromString } from "generated/output/Wallet.PrivateKey";
+
+export default {
+  title: "Components/TrustGraph",
+  component: TrustGraph,
+  argTypes: {},
+  parameters: {
+    previewTabs: {
+      "storybook/docs/panel": { hidden: true },
+    },
+  },
+} as ComponentMeta<typeof TrustGraph>;
+
+const graph: Graph = new Map([
+  [unsafeAddrFromString("A"), [] as TrustNode[]],
+  [unsafeAddrFromString("B"), [] as TrustNode[]],
+  [unsafeAddrFromString("C"), [] as TrustNode[]],
+]);
+
+export const Default = (args): ReactElement => <TrustGraph graph={graph} />;
