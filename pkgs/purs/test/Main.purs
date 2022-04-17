00dc30c343a927cdf36b4e3eaab8e462018e0b36
Michael Bock <me@thought2.de>
2022-04-17 19:27:31 +0200

    Add spec

--- pkgs/purs/test/Main.purs
+++ pkgs/purs/test/Main.purs
@@ -1,20 +1,34 @@
 module Test.Main where
 
 import Prelude
+import CirclesCore.Tests as CirclesCore.Tests
 import Effect (Effect)
+import Effect.Aff (launchAff_)
+import PursDeps.Tests as PursDeps.Tests
+import Test.Data.BigInt as Test.Data.BigInt
+import Test.Spec.Reporter (consoleReporter)
+import Test.Spec.Runner (runSpec)
 import Test.Unit.Main (runTest)
 import Test.Wallet.PrivateKey as Test.Wallet.PrivateKey
-import CirclesCore.Tests as CirclesCore.Tests
-import Test.Data.BigInt as Test.Data.BigInt
-import PursDeps.Tests as PursDeps.Tests
 
 foreign import globalRequires :: {}
 
-main :: Effect Unit
-main =
+mainTestUnit :: Effect Unit
+mainTestUnit =
   runTest do
     Test.Wallet.PrivateKey.tests
     CirclesCore.Tests.tests
     CirclesCore.Tests.tests
     Test.Data.BigInt.tests
     PursDeps.Tests.tests
+
+mainTestSpec :: Effect Unit
+mainTestSpec =
+  launchAff_
+    $ runSpec [ consoleReporter ] do
+        CirclesCore.Tests.spec
+
+main :: Effect Unit
+main = do
+  mainTestUnit
+  mainTestSpec
