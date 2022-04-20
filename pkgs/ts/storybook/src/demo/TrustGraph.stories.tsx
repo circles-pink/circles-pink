19f12d35850665b65354a9cb86df7c32ac4de070
nmauersberg <n.mauersberg@gmail.com>
2022-04-20 17:14:52 +0200

    Simple trust graph example

--- pkgs/ts/storybook/src/demo/TrustGraph.stories.tsx
+++ pkgs/ts/storybook/src/demo/TrustGraph.stories.tsx
@@ -2,7 +2,10 @@ import React, { ReactElement } from "react";
 import { ComponentMeta } from "@storybook/react";
 import { TrustGraph, Graph } from "circles/src/components/TrustGraph";
 import { TrustNode } from "generated/output/CirclesCore";
-import { unsafeAddrFromString } from "generated/output/Wallet.PrivateKey";
+import {
+  Address,
+  unsafeAddrFromString,
+} from "generated/output/Wallet.PrivateKey";
 // import Cytoscape from "cytoscape";
 // import COSEBilkent from "cytoscape-cose-bilkent";
 
@@ -19,10 +22,28 @@ export default {
   },
 } as ComponentMeta<typeof TrustGraph>;
 
+const nodeB: TrustNode = {
+  isIncoming: true,
+  isOutgoing: true,
+  limitPercentageIn: 50,
+  limitPercentageOut: 50,
+  mutualConnections: [],
+  safeAddress: unsafeAddrFromString("B"),
+};
+
+const nodeA: TrustNode = {
+  isIncoming: true,
+  isOutgoing: true,
+  limitPercentageIn: 50,
+  limitPercentageOut: 50,
+  mutualConnections: [],
+  safeAddress: unsafeAddrFromString("A"),
+};
+
 const graph: Graph = new Map([
-  [unsafeAddrFromString("A"), [] as TrustNode[]],
-  [unsafeAddrFromString("B"), [] as TrustNode[]],
-  [unsafeAddrFromString("C"), [] as TrustNode[]],
+  [unsafeAddrFromString("A"), [nodeB]],
+  [unsafeAddrFromString("B"), [nodeA]],
+  [unsafeAddrFromString("C"), [nodeA, nodeB] as TrustNode[]],
 ]);
 
 export const Default = (args): ReactElement => <TrustGraph graph={graph} />;
