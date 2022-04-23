28c48490a26ef0d747c69c3adf914d4748e98640
nmauersberg <n.mauersberg@gmail.com>
2022-04-24 00:32:31 +0200

    CoreToWindow with customizable private key

--- pkgs/ts/circles/src/onboarding/views/Debug.tsx
+++ pkgs/ts/circles/src/onboarding/views/Debug.tsx
@@ -34,19 +34,21 @@ export const Debug = ({ state, act }: DebugProps): ReactElement => {
           </FadeIn>
         </Text>
       }
-      //   interaction={
-      //     <FadeIn orientation={orientation} delay={getDelay()}>
-      //       <Input
-      //         autoFocus
-      //         // indicatorColor={mapIndicatorColors(state.usernameApiResult)}
-      //         type="text"
-      //         value={state.magicWords}
-      //         placeholder={t('debug.magicWordsPlaceholder')}
-      //         onChange={e => act(A._debug(A._setMagicWords(e.target.value)))}
-      //         onKeyPress={e => e.key === 'Enter' && act(A._debug(A._debug(unit)))}
-      //       />
-      //     </FadeIn>
-      //   }
+      interaction={
+        <FadeIn orientation={orientation} delay={getDelay()}>
+          <Input
+            autoFocus
+            // indicatorColor={mapIndicatorColors(state.usernameApiResult)}
+            type="text"
+            value={state.magicWords}
+            placeholder={t('debug.magicWordsPlaceholder')}
+            onChange={e => act(A._debug(A._setMagicWords(e.target.value)))}
+            onKeyPress={e =>
+              e.key === 'Enter' && act(A._debug(A._coreToWindow(unit)))
+            }
+          />
+        </FadeIn>
+      }
       control={
         <FadeIn orientation={orientation} delay={getDelay()}>
           <>
