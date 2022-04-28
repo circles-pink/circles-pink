b9d9e43a559c5770b3f1ee2597ad77262e6f0331
nmauersberg <n.mauersberg@gmail.com>
2022-04-28 12:36:13 +0200

    Button loading indicator

--- pkgs/ts/circles/src/components/forms/Button.tsx
+++ pkgs/ts/circles/src/components/forms/Button.tsx
@@ -1,6 +1,42 @@
 import tw, { css, styled } from 'twin.macro';
 import { darken, lighten } from '../../onboarding/utils/colorUtils';
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
+import { LoadingCircles } from '../LoadingCircles';
+
+// -----------------------------------------------------------------------------
+// UI / Button
+// -----------------------------------------------------------------------------
+
+export type ButtonPrio = 'high' | 'medium' | 'low';
+
+export type ButtonState = 'disabled' | 'loading' | 'enabled';
+
+type ButtonProps = {
+  color?: string;
+  fullWidth?: boolean;
+  state?: ButtonState;
+  children?: ReactNode;
+  onClick?: () => void;
+  prio?: ButtonPrio;
+};
+
+export const Button = (props_: ButtonProps) => {
+  const props = normalizeProps(props_);
+  const buttonColor = mapLoadingColor(props.prio, props.color);
+
+  return (
+    <Button_ {...props}>
+      <ButtonContent>
+        <TextWrapper>{props.children}</TextWrapper>
+        {props.state === 'loading' ? <Loading color={buttonColor} /> : ''}
+      </ButtonContent>
+    </Button_>
+  );
+};
+
+// -----------------------------------------------------------------------------
+// UI / Button_
+// -----------------------------------------------------------------------------
 
 type Button_Props = Required<ButtonProps>;
 
@@ -48,30 +84,28 @@ const Button_ = styled.button<Button_Props>(({ color, fullWidth, prio }) => {
 });
 
 // -----------------------------------------------------------------------------
-// Button
+// UI
 // -----------------------------------------------------------------------------
 
-type ButtonProps = {
+const ButtonContent = styled.div(() => [tw`flex items-center`]);
+const TextWrapper = styled.div(() => [
+  css`
+    margin-right: 10px;
+  `,
+]);
+
+// -----------------------------------------------------------------------------
+// UI / Loading
+// -----------------------------------------------------------------------------
+
+const loadingWidth = 40;
+
+type LoadingProps = {
   color?: string;
-  fullWidth?: boolean;
-  state?: ButtonState;
-  children?: ReactNode;
-  onClick?: () => void;
-  prio?: ButtonPrio;
 };
 
-export type ButtonPrio = 'high' | 'medium' | 'low';
-
-export type ButtonState = 'disabled' | 'loading' | 'enabled';
-
-export const Button = (props_: ButtonProps) => {
-  const props = normalizeProps(props_);
-  return (
-    <Button_ {...props}>
-      {props.children}
-      {props.state === 'loading' ? '...' : ''}
-    </Button_>
-  );
+const Loading = ({ color }: LoadingProps) => {
+  return <LoadingCircles width={40} duration={0.75} color={color} />;
 };
 
 // -----------------------------------------------------------------------------
@@ -87,4 +121,15 @@ const normalizeProps = (props: ButtonProps): Required<ButtonProps> => {
     onClick: props.onClick || (() => {}),
     prio: props.prio || 'medium',
   };
+};
+
+const mapLoadingColor = (prio: ButtonPrio, themeColor: string): string => {
+  switch (prio) {
+    case 'high':
+      return lighten(themeColor);
+    case 'medium':
+      return lighten(darkColor);
+    case 'low':
+      return darkColor;
+  }
 };
