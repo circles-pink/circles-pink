00fe4bf9d9269b44163038ddb4eccefb51946278
Michael Bock <mb@thought2.de>
2022-04-28 18:35:39 +0200

    Test gundb

--- pkgs/purs/src/CirclesCore/Gun.purs
+++ pkgs/purs/src/CirclesCore/Gun.purs
@@ -2,6 +2,7 @@ module CirclesCore.Gun (spec) where
 
 import Prelude
 import Data.Maybe (Maybe(..))
+import Debug (spy)
 import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
 import Gun (get, offline, once, put)
@@ -13,14 +14,16 @@ spec =
   describe "Gun" do
     describe "Put" do
       it "Puts data" do
-        gundb <- liftEffect offline
-        ctx <- liftEffect $ gundb # get "users" # put { name: "John", surname: "Doe" }
-        assertGunResult (gundb # get "users" # once) "John"
+        gundb <- liftEffect $ offline
+        ctx <- liftEffect $ gundb # get "users" # put { name: "John", surnameee: "Doe" }
+        result <- (gundb # get "users" # once)
+        let
+          r = spy "res" result
+        pure unit
 
-assertGunResult :: forall a b. Aff (Maybe { data :: { name :: String | a } | b }) -> String -> Aff Unit
-assertGunResult aff name = aff >>= \res -> bound res name
-  where
-  bound :: forall c d. Maybe { data :: { name :: String | c } | d } -> String -> Aff Unit
-  bound (Just gunVal) expectedName = gunVal.data.name `shouldEqual` expectedName
-
-  bound Nothing _ = fail "No result"
+-- assertGunResult :: forall a b. Aff (Maybe { data :: { name :: String | a } | b }) -> String -> Aff Unit
+-- assertGunResult aff name = aff >>= \res -> bound res name
+--   where
+--   bound :: forall c d. Maybe { data :: { name :: String | c } | d } -> String -> Aff Unit
+--   bound (Just gunVal) expectedName = gunVal.data.name `shouldEqual` expectedName
+--   bound Nothing _ = fail "No result"
