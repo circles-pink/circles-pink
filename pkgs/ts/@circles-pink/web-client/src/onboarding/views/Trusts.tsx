9fcf655773ddbb2be3ebc58f913c9cfd7b294e9c
nmauersberg <n.mauersberg@gmail.com>
2022-05-01 11:17:00 +0200

    Trust screen

--- pkgs/ts/@circles-pink/web-client/src/onboarding/views/Trusts.tsx
+++ pkgs/ts/@circles-pink/web-client/src/onboarding/views/Trusts.tsx
@@ -2,7 +2,7 @@ import * as A from 'generated/output/CirclesPink.Garden.StateMachine.Action';
 import { unit } from 'generated/output/Data.Unit';
 import React, { ReactElement, useContext } from 'react';
 import { Button } from '../../components/forms';
-import { Claim, SubClaim, Text } from '../../components/text';
+import { Claim, SubClaim } from '../../components/text';
 import { DialogCard } from '../../components/DialogCard';
 import { FadeIn } from 'anima-react';
 import { Orientation } from 'anima-react/dist/components/FadeIn';
@@ -10,6 +10,21 @@ import { TrustState } from 'generated/output/CirclesPink.Garden.StateMachine.Sta
 import { getIncrementor } from '../utils/getCounter';
 import { t } from 'i18next';
 import { ThemeContext } from '../../context/theme';
+import Icon from '@mdi/react';
+import {
+  mdiNumeric1CircleOutline,
+  mdiNumeric2CircleOutline,
+  mdiNumeric3CircleOutline,
+  mdiAccountGroup,
+  mdiCashFast,
+} from '@mdi/js';
+import tw from 'twin.macro';
+import { InfoCard } from '../../components/InfoCard';
+import QrCode from 'react-qrcode-svg';
+
+// -----------------------------------------------------------------------------
+// Trusts
+// -----------------------------------------------------------------------------
 
 type TrustsProps = {
   state: TrustState;
@@ -42,12 +57,12 @@ export const Trusts = ({ state, act }: TrustsProps): ReactElement => {
       control={
         <FadeIn orientation={orientation} delay={getDelay()}>
           <>
-            <Button
+            {/* <Button
               color={theme.baseColor}
               onClick={() => act(A._trusts(A._getSafeStatus(unit)))}
             >
               {t('safeStateButton')}
-            </Button>
+            </Button> */}
             {state.isReady ? (
               <Button
                 color={theme.baseColor}
@@ -59,7 +74,105 @@ export const Trusts = ({ state, act }: TrustsProps): ReactElement => {
           </>
         </FadeIn>
       }
+      mainContent={
+        <FlexRow>
+          <FadeIn orientation={'up'} delay={getDelay()}>
+            <InfoCard
+              title={'Trusts'}
+              text={
+                <>
+                  <FadeIn orientation={orientation} delay={getDelay()}>
+                    <Text>{t('trusts.getMoreTruts')}</Text>
+                  </FadeIn>
+                  <FadeIn orientation={orientation} delay={getDelay()}>
+                    <CenterElement>
+                      <QrCode
+                        data={state.user.safeAddress}
+                        height="200"
+                        width="200"
+                        fgColor="gray"
+                        bgColor="white"
+                      />
+                    </CenterElement>
+                  </FadeIn>
+                  <FlexRow>
+                    <FadeIn orientation={orientation} delay={getDelay()}>
+                      <Icon
+                        path={mdiNumeric1CircleOutline}
+                        size={2}
+                        color={
+                          state.trusts.length >= 1 ? theme.baseColor : 'gray'
+                        }
+                      />
+                    </FadeIn>
+                    <FadeIn orientation={orientation} delay={getDelay()}>
+                      <Icon
+                        path={mdiNumeric2CircleOutline}
+                        size={2}
+                        color={
+                          state.trusts.length >= 2 ? theme.baseColor : 'gray'
+                        }
+                      />
+                    </FadeIn>
+                    <FadeIn orientation={orientation} delay={getDelay()}>
+                      <Icon
+                        path={mdiNumeric3CircleOutline}
+                        size={2}
+                        color={
+                          state.trusts.length >= 3 ? theme.baseColor : 'gray'
+                        }
+                      />
+                    </FadeIn>
+                  </FlexRow>
+                </>
+              }
+              themeColor={theme.baseColor}
+              icon={mdiAccountGroup}
+            />
+          </FadeIn>
+
+          <FadeIn orientation={'up'} delay={getDelay()}>
+            <InfoCard
+              title={'Gnosis Safe'}
+              text={
+                <>
+                  <FadeIn orientation={orientation} delay={getDelay()}>
+                    <Text>{t('trusts.fundYourSafe')}</Text>
+                  </FadeIn>
+                  <FadeIn orientation={orientation} delay={getDelay()}>
+                    <CenterElement>
+                      <QrCode
+                        data={state.user.safeAddress}
+                        height="200"
+                        width="200"
+                        fgColor="gray"
+                        bgColor="white"
+                      />
+                    </CenterElement>
+                  </FadeIn>
+                  <FadeIn orientation={orientation} delay={getDelay()}>
+                    <CenterText>
+                      <Text>{state.user.safeAddress}</Text>
+                    </CenterText>
+                  </FadeIn>
+                </>
+              }
+              themeColor={theme.baseColor}
+              icon={mdiCashFast}
+            />
+          </FadeIn>
+        </FlexRow>
+      }
       // // debug={<pre>{JSON.stringify(state, null, 2)}</pre>}
     />
   );
 };
+
+// -----------------------------------------------------------------------------
+// UI
+// -----------------------------------------------------------------------------
+
+const FlexRow = tw.div`flex lg:flex-row flex-col justify-between mb-4`;
+const CenterText = tw.div`text-center`;
+const CenterElement = tw.div`flex justify-around`;
+const Text = tw.p`mt-4 text-lg font-medium text-gray-500`;
