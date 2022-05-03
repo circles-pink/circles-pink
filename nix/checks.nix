3580101975ce2cbd0b8eea7a3cb3162c5b11ff25
Michael Bock <me@thought2.de>
2022-05-03 05:49:19 +0200

    Add nix-fp-lite miraculix

--- nix/checks.nix
+++ nix/checks.nix
@@ -1,4 +1,46 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+
+let
+
+  inherit (builtins) toJSON trace readFile;
+  inherit (pkgs.lib)
+    foldr
+    nameValuePair
+    pipe
+    filterAttrs
+    mapAttrsToList
+    listToAttrs
+    hasPrefix
+    isAttrs
+    mapAttrs
+    length
+    runTests;
+  inherit (pkgs) writeText runCommand;
+
+
+  doTests = as:
+    let
+      failures = runTests as;
+      failureCount = length failures;
+      report = pipe
+        (toJSON failures) [
+        (writeText "report.json")
+        (p: runCommand "format" { } "cat ${p} | ${pkgs.jq}/bin/jq > $out")
+        readFile
+        (txt: "\n${txt}\n")
+      ];
+      out =
+        if failureCount != 0 then
+          abort "${toString failureCount} tests failed"
+        else runCommand "out" { } "mkdir $out";
+    in
+    trace report out;
+
+  doTestSuite = mapAttrs (name: value: pipe (doTests value) [ (trace "Test: ${name}") ]);
+
+in
+
+{
 
   pursTests = pkgs.circles-pink.purs.projectTests;
 
@@ -11,6 +53,12 @@
       ];
     in
     pkgs.runCommand "nix-lint" { } ''
-      ${pkgs.deadnix}/bin/deadnix --fail ${builtins.concatStringsSep " " paths} > $out
+      ${pkgs.deadnix}/bin/deadnix --fail ${builtins.concatStringsSep " " paths}
+      mkdir $out
     '';
-}
+
+  nixUnitTests = runTestsDrv (testGroup "group" [
+    (testCase "test" (isEq 1 2))
+  ]);
+
+} // (doTestSuite pkgs.nix-fp.tests)
---
+++ nix/pkgs/nix-fp-lite.nix
@@ -0,0 +1,102 @@
+{ lib }:
+let
+  inherit (lib) pipe;
+  assertEq = expr: expected: { inherit expr expected; };
+  inherit (type) tag match;
+
+  type =
+    let
+      tag = type: data: { inherit type data; };
+      match = cases: x: cases.${x.type} x.data;
+    in
+    { inherit tag match; };
+
+
+  either =
+    let
+      # Constructors
+
+      left = tag "left";
+      right = tag "right";
+
+      #
+
+      map = f: match { left = left; right = x: right (f x); };
+      bind = f: match { left = left; right = f; };
+
+      # Instances
+
+      Functor = { inherit map; };
+      Bind = { inherit bind; };
+      #Apply = Functor // { inherit apply; };
+      #Applicative = Apply // { inherit pure; };
+      #Monad = Applicative // Bind;
+    in
+    {
+      inherit left right map bind;
+      tests = {
+        # testLeft = assertEq
+        #   (pipe (left 1) [ (match { left = identity; right = const 0; }) ])
+        #   1;
+        # testRight = assertEq
+        #   (pipe (right 1) [ (match { left = const 0; right = identity; }) ])
+        #   1;
+      };
+    };
+
+  function =
+    let
+      identity = x: x;
+      const = _: x: x;
+    in
+    {
+      inherit identity const;
+    };
+
+  Apply =
+    let
+      inherit (function) identity const;
+
+      applySnd = Apply: m1: m2: pipe
+        (Apply.map (const identity) m1) [
+        (f: Apply.apply f m2)
+      ];
+    in
+    { };
+
+  Bind =
+    { };
+
+  io =
+    let
+      inherit (builtins) seq trace;
+
+      map = f: x: _: f (x null); # seq, too?
+      bind = f: x: _: let r = x null; in seq r (f r null);
+      apply = f: x: _: (f null) (x null); # seq, too?
+      pure = x: _: x;
+      bind_ = y: x: _: let r = x null; in seq r (y null);
+
+      # Instances
+
+      Functor = { inherit map; };
+      Bind = { inherit bind; };
+      Apply = Functor // { inherit apply; };
+      Applicative = Apply // { inherit pure; };
+      Monad = Applicative // Bind;
+
+      # Util
+
+      log = s: _: trace s null;
+    in
+    {
+      inherit map bind apply pure Functor Bind Apply Applicative Monad log;
+    };
+
+in
+{
+  inherit either io function Apply type;
+  tests = {
+    either = either.tests;
+  };
+}
