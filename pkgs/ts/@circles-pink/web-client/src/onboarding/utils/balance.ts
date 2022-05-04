a5acd6e5b3dde5c3369b696362c42da438a2ca23
nmauersberg <n.mauersberg@gmail.com>
2022-05-04 15:56:00 +0200

    Balance wip

--- pkgs/ts/@circles-pink/web-client/src/onboarding/utils/balance.ts
+++ pkgs/ts/@circles-pink/web-client/src/onboarding/utils/balance.ts
@@ -1,13 +1,13 @@
 import { Balance } from 'generated/output/CirclesCore.Bindings';
 
-export const mapBalanceToBN = (raw: number) => {
+export const mapBalanceToBN = (raw: number): string => {
   const rawBalance = (raw * 100).toString();
   // Map and round balance to big number format
   // Todo: Replace! Ugly but works for the moment...
   return rawBalance + '0000000000000000';
 };
 
-export const mapBalanceToHr = (raw: Balance) => {
+export const mapBalanceToHr = (raw: Balance): string => {
   const rawBalance = parseInt(raw.toString());
   // Map and round balance to human readable format
   return (
---
+++ pkgs/ts/@circles-pink/web-client/src/onboarding/views/dashboard/Balance.tsx
@@ -0,0 +1,59 @@
+import { TokenGetBalanceResult } from 'generated/output/CirclesPink.Garden.StateMachine.State';
+import React from 'react';
+import tw, { css, styled } from 'twin.macro';
+import { CirclesCurrency } from '../../../assets/CirclesCurrency';
+import { Theme } from '../../../context/theme';
+import { mapBalanceToHr } from '../../utils/balance';
+
+type BalanceProps = {
+  theme: Theme;
+  balance: TokenGetBalanceResult;
+};
+export const Balance = ({ theme, balance }: BalanceProps) => {
+  switch (balance.type) {
+    case 'notAsked':
+      return (
+        <BalanceWrapper>
+          <Amount color={theme.baseColor}>0.00</Amount>
+          <CirclesCurrency color={theme.baseColor} />
+        </BalanceWrapper>
+      );
+    case 'failure':
+      return (
+        <BalanceWrapper>
+          <Amount color={theme.baseColor}>0.00</Amount>
+          <CirclesCurrency color={theme.baseColor} />
+        </BalanceWrapper>
+      );
+    case 'loading':
+      return (
+        <BalanceWrapper>
+          <Amount color={theme.baseColor}>0.00</Amount>
+          <CirclesCurrency color={theme.baseColor} />
+        </BalanceWrapper>
+      );
+    case 'success':
+      return (
+        <BalanceWrapper>
+          <Amount color={theme.baseColor}>
+            {mapBalanceToHr(balance.value)}
+          </Amount>
+          <CirclesCurrency color={theme.baseColor} />
+        </BalanceWrapper>
+      );
+  }
+};
+
+// -----------------------------------------------------------------------------
+// UI / Balance
+// -----------------------------------------------------------------------------
+
+type AmountProps = { color?: string };
+
+const Amount = styled.h2<AmountProps>(({ color }) => [
+  tw`text-5xl mr-2 my-2`,
+  css`
+    color: ${color || 'black'};
+  `,
+]);
+const BalanceWrapper = tw.div`flex flex-row items-center m-2`;
