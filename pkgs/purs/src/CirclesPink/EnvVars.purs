3e7ea1da5cb55c2d9e78ee68917bf7cb5676773c
Michael Bock <me@thought2.de>
2022-05-10 06:02:14 +0200

    Finalize EnvVars

--- pkgs/purs/src/CirclesPink/EnvVars.purs
+++ pkgs/purs/src/CirclesPink/EnvVars.purs
@@ -3,6 +3,8 @@ module CirclesPink.EnvVars where
 import Prelude
 import CirclesPink.URI (URI)
 import Control.Monad.Except (ExceptT(..))
+import Data.Either (Either)
+import Effect (Effect)
 import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
 import Network.Ethereum.Core.HexString (mkHexString)
@@ -16,14 +18,14 @@ import TypedEnv (fromEnv) as TypedEnv
 type Config :: forall k. (Symbol -> Type -> k) -> Row k
 type Config f
   = ( gardenApi :: f "GARDEN_API" URI
-    , gardenApiUsers :: f "GARDEN_API_USERS" String
-    , gardenGraphApi :: f "GARDEN_GRAPH_API" String
+    , gardenApiUsers :: f "GARDEN_API_USERS" URI
+    , gardenGraphApi :: f "GARDEN_GRAPH_API" URI
     , gardenSubgraphName :: f "GARDEN_SUBGRAPH_NAME" String
-    , gardenRelay :: f "GARDEN_RELAY" String
+    , gardenRelay :: f "GARDEN_RELAY" URI
     , gardenHubAdress :: f "GARDEN_HUB_ADDRESS" Address
-    , gardenProxyFactoryAdress :: f "GARDEN_PROXY_FACTORY_ADRESS" String
-    , gardenSafeMasterAddress :: f "GARDEN_SAFE_MASTER_ADDRESS" String
-    , gardenEthereumNodeWs :: f "GARDEN_ETHEREUM_NODE_WS" String
+    , gardenProxyFactoryAdress :: f "GARDEN_PROXY_FACTORY_ADRESS" Address
+    , gardenSafeMasterAddress :: f "GARDEN_SAFE_MASTER_ADDRESS" Address
+    , gardenEthereumNodeWs :: f "GARDEN_ETHEREUM_NODE_WS" URI
     )
 
 --------------------------------------------------------------------------------
@@ -35,13 +37,13 @@ newtype Address
 instance parseValueAddress :: ParseValue Address where
   parseValue x = mkHexString x >>= mkAddress <#> Address
 
+derive newtype instance showAddress :: Show Address
+
 --------------------------------------------------------------------------------
 type ResolvedConfig
   = Record (Config Resolved)
 
-parse :: ExceptT EnvError Aff ResolvedConfig
-parse =
+getParsedEnv :: Effect (Either EnvError ResolvedConfig)
+getParsedEnv =
   TypedEnv.fromEnv (Proxy :: _ (Config Variable))
     <$> getEnv
-    # liftEffect
-    # ExceptT
