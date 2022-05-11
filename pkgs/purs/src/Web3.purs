b3b92bf5891c0f5e92a1a6b6b9be3886de94de0e
Michael Bock <mb@thought2.de>
2022-05-11 17:44:06 +0200

    Finish FFI

--- pkgs/purs/src/Web3.purs
+++ pkgs/purs/src/Web3.purs
@@ -1,9 +1,20 @@
-module Web3 where
+module Web3
+  ( sendTransaction
+  ) where
 
 import Prelude
-import Web3.Bindings as B
+import Data.Maybe (fromJust)
 import Effect.Aff (Aff)
 import Effect.Aff.Compat (fromEffectFnAff)
+import Network.Ethereum.Core.HexString (HexString, mkHexString)
+import Network.Ethereum.Core.Signatures as W3
+import Partial.Unsafe (unsafePartial)
+import Web3.Bindings as B
 
-sendTransaction :: { from :: String, to :: String, value :: Number } -> Aff String
-sendTransaction opts = fromEffectFnAff $ B.sendTransaction opts
+sendTransaction :: { from :: W3.Address, to :: W3.Address, value :: Number } -> Aff HexString
+sendTransaction opts =
+  unsafePartial
+    ( B.sendTransaction { from: show opts.from, to: show opts.to, value: opts.value }
+        # fromEffectFnAff
+        <#> (mkHexString >>> fromJust)
+    )
