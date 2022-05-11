bd256ad141c61f0c80b76e7662293925a9bf5615
Michael Bock <mb@thought2.de>
2022-05-11 09:58:31 +0200

    Make Addresses compatible

--- pkgs/purs/src/Network/Ethereum/Core/Signatures/Extra.purs
+++ pkgs/purs/src/Network/Ethereum/Core/Signatures/Extra.purs
@@ -5,10 +5,16 @@ module Network.Ethereum.Core.Signatures.Extra
   ) where
 
 import Prelude
+import Convertable (class Convertible, convert)
 import Data.Maybe (fromJust)
-import Network.Ethereum.Core.HexString (HexString, mkHexString)
+import Network.Ethereum.Core.HexString (HexString, mkHexString, unHex)
+import Network.Ethereum.Core.Signatures (Address, mkAddress)
+import Network.Ethereum.Core.Signatures as W3
+import Partial.Unsafe (unsafePartial)
 import TypedEnv (class ParseValue)
 import Undefined (undefined)
+import Wallet.PrivateKey as C
+import Web3.Bindings (web3)
 
 newtype ChecksumAddress
   = ChecksumAddress HexString
@@ -20,4 +26,30 @@ unChecksumAddress :: ChecksumAddress -> HexString
 unChecksumAddress (ChecksumAddress h) = h
 
 instance parseValueAddress :: ParseValue ChecksumAddress where
-  parseValue x = undefined
+  parseValue x = mkHexString x >>= mkAddress <#> convert
+
+--------------------------------------------------------------------------------
+instance convertible_ChecksumAddress_CAddress :: Convertible ChecksumAddress C.Address where
+  convert p =
+    unsafePartial
+      (p # unChecksumAddress # unHex # C.unsafeAddrFromString)
+
+instance convertible_ChecksumAddress_W3Address :: Convertible ChecksumAddress W3.Address where
+  convert p =
+    unsafePartial
+      (p # unChecksumAddress # W3.mkAddress # fromJust)
+
+instance convertible_W3Address_ChecksumAddress :: Convertible W3.Address ChecksumAddress where
+  convert p =
+    unsafePartial
+      ( p
+          # W3.unAddress
+          # unHex
+          # web3.utils.toChecksumAddress
+          # unsafeFromString
+      )
+
+instance convertible_CAddress_ChecksumAddress :: Convertible C.Address ChecksumAddress where
+  convert p =
+    unsafePartial
+      (p # C.addrToString # web3.utils.toChecksumAddress # unsafeFromString)
