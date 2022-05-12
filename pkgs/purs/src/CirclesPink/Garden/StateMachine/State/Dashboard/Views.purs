c189b47025108fc2d401cca7e38343d78a40e98a
Michael Bock <mb@thought2.de>
2022-05-12 15:58:18 +0200

    Add dashboard state views

--- pkgs/purs/src/CirclesPink/Garden/StateMachine/State/Dashboard/Views.purs
+++ pkgs/purs/src/CirclesPink/Garden/StateMachine/State/Dashboard/Views.purs
@@ -4,9 +4,16 @@ module CirclesPink.Garden.StateMachine.State.Dashboard.Views
 
 import Prelude
 import CirclesPink.Garden.StateMachine.State (DashboardState)
+import CirclesPink.Garden.StateMachine.State.Dashboard as D
 import Data.Array (any)
+import Data.Map as M
+import Data.Tuple.Nested (type (/\), (/\))
 import Foreign.Object (values)
+import Network.Ethereum.Core.Signatures as W3
+import Record (merge)
 import RemoteData (isLoading)
+import Undefined (undefined)
+import Wallet.PrivateKey (Address(..))
 
 anythingLoading :: DashboardState -> Boolean
 anythingLoading d = any (_ == true) $ join checks
@@ -24,3 +31,34 @@ anythingLoading d = any (_ == true) $ join checks
     , pure $ check d.transferResult
     , pure $ check d.userSearchResult
     ]
+
+--------------------------------------------------------------------------------
+type DefaultView
+  = { trusts :: Trusts
+    }
+
+type Trusts
+  = Array Trust
+
+type Trust
+  = { safeAddress :: String
+    , isLoading :: Boolean
+    , isIncoming :: Boolean
+    , isOutgoing :: Boolean
+    , id :: Int
+    , username :: String
+    , avatarUrl :: String
+    }
+
+mapTrust :: W3.Address /\ D.Trust -> Trust
+mapTrust (a /\ t) = t `merge` { safeAddress: show a }
+
+mapTrusts :: D.Trusts -> Trusts
+mapTrusts xs = M.toUnfoldable xs <#> mapTrust
+
+defaultView :: DashboardState -> DefaultView
+defaultView { trusts } =
+  { trusts: mapTrusts trusts
+  }
+
+--------------------------------------------------------------------------------
