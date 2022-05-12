fe9f06d787a7b6443a17e5eb2cd38a8cc9b474ff
nmauersberg <n.mauersberg@gmail.com>
2022-05-12 18:50:34 +0200

    Add test

---
+++ pkgs/purs/test/CirclesPink/Garden/StateMachine/Control/States/Dashboard/Tests.purs
@@ -0,0 +1,12 @@
+module CirclesPink.Garden.StateMachine.Control.States.Dashboard.Tests where
+
+import Prelude
+import Test.Spec (Spec, describe, it)
+import Test.Spec.Assertions (shouldEqual)
+
+spec :: Spec Unit
+spec =
+  describe "CirclesPink.Garden.StateMachine.Control.States.Dashboard" do
+    describe "fetchUsersBinarySearch" do
+      it "..." do
+        1 `shouldEqual` 1
