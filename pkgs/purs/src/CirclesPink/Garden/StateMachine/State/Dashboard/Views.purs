a9b854a1cba9ee8d3d8266c0be8e014658b43fe3
Michael Bock <me@thought2.de>
2022-05-13 04:59:50 +0200

    Start to move Resolved errors to Views

--- pkgs/purs/src/CirclesPink/Garden/StateMachine/State/Dashboard/Views.purs
+++ pkgs/purs/src/CirclesPink/Garden/StateMachine/State/Dashboard/Views.purs
@@ -1,22 +1,24 @@
 module CirclesPink.Garden.StateMachine.State.Dashboard.Views
   ( DefaultView
+  , ErrUserSearchResolved
   , Trust
   , Trusts
   , defaultView
   ) where
 
 import Prelude
-import CirclesCore (User)
+import CirclesCore (ApiError, User, NativeError)
 import CirclesPink.Garden.StateMachine.State (DashboardState)
 import CirclesPink.Garden.StateMachine.State.Dashboard as D
 import Data.Array (any)
 import Data.Map as M
 import Data.Nullable (Nullable, toNullable)
 import Data.Tuple.Nested (type (/\), (/\))
+import Data.Variant (Variant)
 import Foreign.Object (values)
 import Network.Ethereum.Core.Signatures as W3
 import Record (merge)
-import RemoteData (isLoading)
+import RemoteData (RemoteData(..), isLoading)
 import Undefined (undefined)
 import Wallet.PrivateKey (Address(..))
 
@@ -40,6 +42,7 @@ anythingLoading d = any (_ == true) $ join checks
 --------------------------------------------------------------------------------
 type DefaultView
   = { trusts :: Trusts
+    , userSearchResult :: RemoteData Unit Unit ErrUserSearchResolved (Array User)
     }
 
 type Trusts
@@ -66,8 +69,15 @@ mapTrusts :: D.Trusts -> Trusts
 mapTrusts xs = M.toUnfoldable xs <#> mapTrust
 
 defaultView :: DashboardState -> DefaultView
-defaultView { trusts } =
+defaultView d@{ trusts } =
   { trusts: mapTrusts trusts
+  , userSearchResult: d.userSearchResult
   }
 
 --------------------------------------------------------------------------------
+type ErrUserSearchResolved
+  = Variant
+      ( errApi :: ApiError
+      , errNative :: NativeError
+      , errInvalidUrl :: String
+      )
