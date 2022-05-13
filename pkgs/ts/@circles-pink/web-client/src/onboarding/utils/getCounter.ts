e89c9787ee4a2ad68b60929b1a852eb34f96a5b9
Michael Bock <mb@thought2.de>
2022-05-13 17:52:40 +0200

    Build babel with nix

--- pkgs/ts/@circles-pink/web-client/src/onboarding/utils/getCounter.ts
+++ pkgs/ts/@circles-pink/web-client/src/onboarding/utils/getCounter.ts
@@ -1,11 +1,11 @@
 export const getCounter = (init: number) => {
-    let counter = init;
-    return (): number  => {
-        return ++counter;
-    };
+  let counter = init;
+  return (): number => {
+    return ++counter;
+  };
 };
 
 export const getIncrementor = (init: number, increment: number) => {
-    const counter = getCounter(init)
-    return () => counter() * increment;
+  const counter = getCounter(init);
+  return () => counter() * increment;
 };
