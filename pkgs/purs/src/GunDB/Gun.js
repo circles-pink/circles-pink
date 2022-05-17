030ff85276015f8750c8538d5ca9820db8c4b1ed
nmauersberg <n.mauersberg@gmail.com>
2022-05-17 16:51:52 +0200

    Refactor gun bindings, remove unused cancelers

--- pkgs/purs/src/GunDB/Gun.js
+++ pkgs/purs/src/GunDB/Gun.js
@@ -4,134 +4,77 @@
 
 var Gun = require("gun");
 
-exports.syncWithPeer = function (url) {
-  return function () {
-    return Gun(url);
-  };
+exports.syncWithPeer = (url) => () => Gun(url);
+
+exports._on = (ctx) => (onErr, onSucc) => {
+  try {
+    ctx.on((data, key) => {
+      onSucc({ data: data, key: key });
+    });
+  } catch (error) {
+    onErr(error);
+  }
 };
 
-exports._on = function (ctx) {
-  return function (onError, onSuccess) {
-    var canceled = false;
-    ctx.on(function (data, key) {
-      if (!canceled) {
-        onSuccess({ data: data, key: key });
+exports.syncWithPeers = (urls) => () => Gun(urls);
+
+exports.offline = () => Gun();
+
+exports._getOnGunDb = (pathElements) => (ctx) => {
+  var arrayLength = pathElements.length;
+  for (var i = 0; i < arrayLength; i++) {
+    ctx = ctx.get(pathElements[i]);
+  }
+  return ctx;
+};
+
+exports._getOnUser = (pathElements) => (ctx) => {
+  var arrayLength = pathElements.length;
+  for (var i = 0; i < arrayLength; i++) {
+    ctx = ctx.get(pathElements[i]);
+  }
+  return ctx;
+};
+
+exports.map = (mapper) => (ctx) => ctx.map(mapper);
+
+exports.each = (ctx) => ctx.map();
+
+exports.filter = (filter) => (ctx) =>
+  ctx.map((e) => (filter(e) ? e : undefined));
+
+exports.put = (data) => (ctx) => () => ctx.put(data);
+
+exports.set = (ref) => (ctx) => () => ctx.set(ref);
+
+exports._once = (Nothing) => (Just) => (ctx) => (onErr, onSucc) => {
+  ctx.once((data, key) => {
+    try {
+      if (data === undefined) {
+        onSucc(Nothing);
+      } else {
+        onSucc(Just({ data: data, key: key }));
       }
-    });
-    return function (cancelError, cancelerError, cancelerSuccess) {
-      canceled = true;
-      cancelerSuccess();
-    };
-  };
-};
-
-exports.syncWithPeers = function (urls) {
-  return function () {
-    return Gun(urls);
-  };
-};
-
-exports.offline = function () {
-  return Gun();
-};
-
-exports._getOnGunDb = function (pathElements) {
-  return function (ctx) {
-    var arrayLength = pathElements.length;
-    for (var i = 0; i < arrayLength; i++) {
-      ctx = ctx.get(pathElements[i]);
+    } catch (error) {
+      onErr(error);
     }
-    return ctx;
-  };
+  });
 };
 
-exports._getOnUser = function (pathElements) {
-  return function (ctx) {
-    var arrayLength = pathElements.length;
-    for (var i = 0; i < arrayLength; i++) {
-      ctx = ctx.get(pathElements[i]);
-    }
-    return ctx;
-  };
-};
-
-exports.map = function (mapper) {
-  return function (ctx) {
-    return ctx.map(mapper);
-  };
-};
-
-exports.each = function (ctx) {
-  return ctx.map();
-};
-
-exports.filter = function (filter) {
-  return function (ctx) {
-    return ctx.map(function (e) {
-      return filter(e) ? e : undefined;
-    });
-  };
-};
-
-exports.put = function (data) {
-  return function (ctx) {
-    return function () {
-      return ctx.put(data);
-    };
-  };
-};
-
-exports.set = function (ref) {
-  return function (ctx) {
-    return function () {
-      return ctx.set(ref);
-    };
-  };
-};
-
-exports._load = (Nothing) => (Just) => {
-  return function (waittime) {
-    return function (ctx) {
-      return function (onError, onSuccess) {
-        var canceled = false;
-        ctx.load(
-          function (data) {
-            if (!canceled) {
-              if (data === undefined) {
-                onSuccess(Nothing);
-              } else {
-                onSuccess(Just(data));
-              }
-            }
-          },
-          { wait: waittime }
-        );
-        return function (cancelError, cancelerError, cancelerSuccess) {
-          canceled = true;
-          cancelerSuccess();
-        };
-      };
-    };
-  };
-};
-
-exports._once = (Nothing) => (Just) => {
-  return function (ctx) {
-    return function (onError, onSuccess) {
-      var canceled = false;
-      ctx.once(function (data, key) {
-        if (!canceled) {
+exports._load =
+  (Nothing) => (Just) => (waittime) => (ctx) => (onErr, onSucc) => {
+    try {
+      ctx.load(
+        (data) => {
           if (data === undefined) {
-            onSuccess(Nothing);
+            onSucc(Nothing);
           } else {
-            onSuccess(Just({ data: data, key: key }));
+            onSucc(Just(data));
           }
-        }
-      });
-      return function (cancelError, cancelerError, cancelerSuccess) {
-        canceled = true;
-        cancelerSuccess();
-      };
-    };
+        },
+        { wait: waittime }
+      );
+    } catch (error) {
+      onErr(error);
+    }
   };
-};
