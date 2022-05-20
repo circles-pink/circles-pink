154d6fad3f611e1c16c73532f7f19d5c8ab97d7f
Michael Bock <mb@thought2.de>
2022-05-20 12:11:46 +0200

    Write test for userSignup

--- pkgs/purs/test/CirclesPink/Garden/StateMachine/Stories/Tests.purs
+++ pkgs/purs/test/CirclesPink/Garden/StateMachine/Stories/Tests.purs
@@ -4,13 +4,36 @@ module CirclesPink.Garden.StateMachine.Stories.Tests
 
 import Prelude
 
+import CirclesPink.Garden.Env (testEnv)
+import CirclesPink.Garden.StateMachine.State (CirclesState)
+import CirclesPink.Garden.StateMachine.State as S
+import CirclesPink.Garden.StateMachine.Stories (ScriptT, runScripT, signUpUser)
+import Control.Monad.Identity.Trans (runIdentityT)
+import Data.Bifunctor (lmap)
+import Data.Either (Either(..))
+import Data.Identity (Identity(..))
+import Data.Newtype (unwrap)
+import Data.Tuple (fst, snd)
+import Data.Variant.Extra (getLabel)
 import Test.Spec (Spec, describe, it)
-import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Assertions (shouldEqual, shouldSatisfy)
+
+--------------------------------------------------------------------------------
+
+type AppM e a = ScriptT e Identity a
+
+execAppM :: forall e a. AppM e a -> CirclesState
+execAppM x = x # runScripT # unwrap # snd
+
+--------------------------------------------------------------------------------
 
 spec :: Spec Unit
 spec =
   describe "CirclesPink.Garden.StateMachine.Stories" do
-    describe "bar" do
-      it "foo" do
-        1
-          # shouldEqual 2
+    describe "A user can signup" do
+      it "ends up in `trusts` state" do
+        ( signUpUser testEnv { username: "Foo", email: "foo@bar.com" }
+            # execAppM
+            # getLabel
+        )
+          `shouldEqual` "trusts"
