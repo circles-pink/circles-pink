6e81e3cc8ff1137437c530dc3306171c4379956b
Michael Bock <mb@thought2.de>
2022-05-25 18:08:43 +0200

    Remove comment

--- pkgs/purs/src/CirclesPink/Garden/StateMachine/Control/States/Trusts.purs
+++ pkgs/purs/src/CirclesPink/Garden/StateMachine/Control/States/Trusts.purs
@@ -55,28 +55,3 @@ trusts env@{ deployToken, deploySafe } =
             # dropError
           lift $ set \_ -> S.initDashboard
             { user: st.user, privKey: st.privKey }
-
--- do
---   set \st' -> S._trusts st' { trustsResult = _loading unit }
---   let
---     task :: ExceptV S.ErrTrustState _ _
---     task = do
---       -- First deploy always fails
---       _ <- env.deploySafe st.privKey `catchError` \_ -> pure unit
---       _ <- (env.deployToken st.privKey <#> const unit) `catchError` \_ -> pure unit
---       -- Second deploy works most of the time
---       _ <- env.deploySafe st.privKey `catchError` \_ -> pure unit
---       _ <- (env.deployToken st.privKey <#> const unit) `catchError` \_ -> pure unit
---       -- Make really sure, the token is deployed!
---       _ <- env.deploySafe st.privKey
---       _ <- (env.deployToken st.privKey <#> const unit)
---       pure unit
---   results <- runExceptT' task
---   case results of
---     Left e -> set \st' -> S._trusts st' { trustsResult = _failure e }
---     Right _ ->
---       set \_ ->
---         S.initDashboard
---           { user: st.user
---           , privKey: st.privKey
---           }
