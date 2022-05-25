af32bb577101e5aa6f75c55657be132c1126826a
Michael Bock <mb@thought2.de>
2022-05-25 19:10:07 +0200

    1.0.29

---
+++ pkgs/purs/src/CirclesPink/Garden/ApiSimulation.purs
@@ -0,0 +1,28 @@
+module CirclesPink.Garden.ApiSimulation where
+
+import Prelude
+
+-- import Control.Monad.Gen (class MonadGen, chooseInt)
+-- import Control.Monad.State (class MonadState, get)
+-- import Data.Map (Map)
+-- import Data.Typelevel.Undefined (undefined)
+-- import Network.Ethereum.Core.Signatures (Address)
+
+-- type SimulationState =
+--   { users ::
+--       Map Address
+--         { username :: String
+--         , email :: String
+--         }
+--   , newUser ::
+--       {
+--       }
+--   }
+
+-- class (MonadState SimulationState m, MonadGen m) <= SimC m
+
+-- init :: forall m. SimC m => m Unit
+-- init = do
+--   s <- get
+--   r <- chooseInt 1 2
+--   pure unit
