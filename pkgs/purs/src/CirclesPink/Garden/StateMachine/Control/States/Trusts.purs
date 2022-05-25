a89ed1f04b69a1f24f4a621219b12937b2baa394
Michael Bock <mb@thought2.de>
2022-05-25 18:08:11 +0200

    Refactor finalizeAccount and add test

--- pkgs/purs/src/CirclesPink/Garden/StateMachine/Control/States/Trusts.purs
+++ pkgs/purs/src/CirclesPink/Garden/StateMachine/Control/States/Trusts.purs
@@ -4,13 +4,14 @@ module CirclesPink.Garden.StateMachine.Control.States.Trusts
 
 import Prelude
 
-import CirclesPink.Garden.StateMachine.Control.Common (ActionHandler', readyForDeployment, runExceptT')
+import CirclesPink.Garden.StateMachine.Control.Common (ActionHandler', dropError, readyForDeployment, retryUntil, runExceptT', subscribeRemoteReport)
 import CirclesPink.Garden.StateMachine.Control.Env as Env
 import CirclesPink.Garden.StateMachine.State as S
-import Control.Monad.Except (catchError)
+import Control.Monad.Except (catchError, lift, runExceptT)
 import Control.Monad.Except.Checked (ExceptV)
-import Data.Either (Either(..))
+import Data.Either (Either(..), isRight)
 import RemoteData (_failure, _loading)
+import Undefined (undefined)
 
 trusts
   :: forall m
@@ -19,7 +20,7 @@ trusts
   -> { getSafeStatus :: ActionHandler' m Unit S.TrustState ("trusts" :: S.TrustState)
      , finalizeRegisterUser :: ActionHandler' m Unit S.TrustState ("trusts" :: S.TrustState, "dashboard" :: S.DashboardState)
      }
-trusts env =
+trusts env@{ deployToken, deploySafe } =
   { getSafeStatus
   , finalizeRegisterUser
   }
@@ -34,30 +35,48 @@ trusts env =
         pure { safeStatus, isReady: isReady', trusts: trusts' }
     results <- runExceptT' task
     case results of
-      Left e -> set \st' -> S._trusts st' { trustsResult = _failure e }
+      Left e -> set \st' -> S._trusts st' { trustsResult = _failure { error: e, timestamp: bottom, retry: 0 } }
       Right r -> set \st' -> S._trusts st' { safeStatus = r.safeStatus, isReady = r.isReady, trusts = r.trusts }
 
-  finalizeRegisterUser set st _ = do
-    set \st' -> S._trusts st' { trustsResult = _loading unit }
+  finalizeRegisterUser set st _ =
     let
-      task :: ExceptV S.ErrTrustState _ _
-      task = do
-        -- First deploy always fails
-        _ <- env.deploySafe st.privKey `catchError` \_ -> pure unit
-        _ <- (env.deployToken st.privKey <#> const unit) `catchError` \_ -> pure unit
-        -- Second deploy works most of the time
-        _ <- env.deploySafe st.privKey `catchError` \_ -> pure unit
-        _ <- (env.deployToken st.privKey <#> const unit) `catchError` \_ -> pure unit
-        -- Make really sure, the token is deployed!
-        _ <- env.deploySafe st.privKey
-        _ <- (env.deployToken st.privKey <#> const unit)
+      doDeploys = do
+        _ <- (\_ -> deploySafe st.privKey)
+          # retryUntil env (const { delay: 1000 }) (\r _ -> isRight r) 0
+        _ <- (\_ -> deployToken st.privKey)
+          # retryUntil env (const { delay: 1000 }) (\r _ -> isRight r) 0
         pure unit
-    results <- runExceptT' task
-    case results of
-      Left e -> set \st' -> S._trusts st' { trustsResult = _failure e }
-      Right _ ->
-        set \_ ->
-          S.initDashboard
-            { user: st.user
-            , privKey: st.privKey
-            }
+    in
+      void do
+        runExceptT do
+          _ <- doDeploys
+            # subscribeRemoteReport env (\r -> set \st' -> S._trusts st' { trustsResult = r })
+            # (\x -> x 0)
+            # dropError
+          lift $ set \_ -> S.initDashboard
+            { user: st.user, privKey: st.privKey }
+
+-- do
+--   set \st' -> S._trusts st' { trustsResult = _loading unit }
+--   let
+--     task :: ExceptV S.ErrTrustState _ _
+--     task = do
+--       -- First deploy always fails
+--       _ <- env.deploySafe st.privKey `catchError` \_ -> pure unit
+--       _ <- (env.deployToken st.privKey <#> const unit) `catchError` \_ -> pure unit
+--       -- Second deploy works most of the time
+--       _ <- env.deploySafe st.privKey `catchError` \_ -> pure unit
+--       _ <- (env.deployToken st.privKey <#> const unit) `catchError` \_ -> pure unit
+--       -- Make really sure, the token is deployed!
+--       _ <- env.deploySafe st.privKey
+--       _ <- (env.deployToken st.privKey <#> const unit)
+--       pure unit
+--   results <- runExceptT' task
+--   case results of
+--     Left e -> set \st' -> S._trusts st' { trustsResult = _failure e }
+--     Right _ ->
+--       set \_ ->
+--         S.initDashboard
+--           { user: st.user
+--           , privKey: st.privKey
+--           }
--- pkgs/purs/test/CirclesPink/Garden/StateMachine/Stories/Tests.purs
+++ pkgs/purs/test/CirclesPink/Garden/StateMachine/Stories/Tests.purs
@@ -5,10 +5,12 @@ module CirclesPink.Garden.StateMachine.Stories.Tests
 import Prelude
 
 import CirclesPink.Garden.Env (liftEnv, testEnv)
+import CirclesPink.Garden.StateMachine.Control.Env (Env)
 import CirclesPink.Garden.StateMachine.State (CirclesState)
 import CirclesPink.Garden.StateMachine.State as S
-import CirclesPink.Garden.StateMachine.Stories (ScriptT, runScripT, signUpUser)
+import CirclesPink.Garden.StateMachine.Stories (ScriptT, finalizeAccount, runScripT, signUpUser)
 import Control.Monad.Identity.Trans (runIdentityT)
+import Control.Monad.State (StateT(..))
 import Data.Bifunctor (lmap)
 import Data.Either (Either(..))
 import Data.Identity (Identity(..))
@@ -29,11 +31,26 @@ execAppM x = x # runScripT # unwrap # snd
 
 spec :: Spec Unit
 spec =
-  describe "CirclesPink.Garden.StateMachine.Stories" do
-    describe "A user can signup" do
-      it "ends up in `trusts` state" do
-        ( signUpUser (liftEnv testEnv ) { username: "Foo", email: "foo@bar.com" }
-            # execAppM
-            # getLabel
-        )
-          `shouldEqual` "trusts"
+  let
+    env' :: Env (StateT CirclesState Identity)
+    env' = liftEnv testEnv
+  in
+    describe "CirclesPink.Garden.StateMachine.Stories" do
+      describe "A user can signup" do
+        it "ends up in `trusts` state" do
+          ( signUpUser (liftEnv testEnv) { username: "Foo", email: "foo@bar.com" }
+              # execAppM
+              # getLabel
+          )
+            `shouldEqual` "trusts"
+      describe "A user can finalize the account" do
+        it "ends up in `dashboard` state" do
+          ( ( do
+                _ <- signUpUser (liftEnv testEnv) { username: "Foo", email: "foo@bar.com" }
+                _ <- finalizeAccount (liftEnv testEnv)
+                pure unit
+            )
+              # execAppM
+              # getLabel
+          )
+            `shouldEqual` "dashboard"
