8cad9f8a4339e6c9467bdb7608396b14cbe97954
Michael Bock <mb@thought2.de>
2022-05-25 16:58:31 +0200

    Add docs to public dir

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -195,6 +195,8 @@
 
       publicDir = { envVars }: final.runCommand "output" { } ''
         cp -r ${ts.builds.storybook {inherit envVars;}} $out
+        chmod -R +w $out
+        cp -r ${purs.docs}/ $out/purs-docs
       '';
 
       runGarden = { envVars }: final.writeShellScriptBin "run-garden" ''
