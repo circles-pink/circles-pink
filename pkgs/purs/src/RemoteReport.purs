b9c5c724f81fe62951a728490744814b262cd1b6
Michael Bock <mb@thought2.de>
2022-05-25 10:46:07 +0200

    Fix RemoteReport

--- pkgs/purs/src/RemoteReport.purs
+++ pkgs/purs/src/RemoteReport.purs
@@ -3,10 +3,13 @@ module RemoteReport
   ) where
 
 import Prelude
+
 import Data.DateTime.Instant (Instant)
+import Data.Maybe (Maybe)
 import RemoteData (RemoteData)
 
-type RemoteReport e a = RemoteData Unit
-  { timestamp :: Instant, retry :: Int }
+type RemoteReport e a = RemoteData
+  Unit
+  { timestamp :: Instant, retry :: Int, previousData :: Maybe a }
   { error :: e, timestamp :: Instant, retry :: Int }
   { data :: a, timestamp :: Instant, retry :: Int }
