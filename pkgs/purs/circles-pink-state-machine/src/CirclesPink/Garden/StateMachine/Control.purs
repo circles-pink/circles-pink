7e7316957042173ef26ae98136c41c13041f4704
Michael Bock <mb@thought2.de>
2022-06-16 11:43:28 +0200

    Add CirclesConfig

--- pkgs/purs/circles-pink-state-machine/src/CirclesPink/Garden/StateMachine/Control.purs
+++ pkgs/purs/circles-pink-state-machine/src/CirclesPink/Garden/StateMachine/Control.purs
@@ -4,30 +4,34 @@ import Prelude
 
 import CirclesPink.Garden.StateMachine (_circlesStateMachine)
 import CirclesPink.Garden.StateMachine.Action (CirclesAction)
+import CirclesPink.Garden.StateMachine.Config (CirclesConfig)
 import CirclesPink.Garden.StateMachine.Control.Env as Env
 import CirclesPink.Garden.StateMachine.Control.States as States
 import CirclesPink.Garden.StateMachine.State as S
 import Stadium.Control as C
 
-circlesControl
-  :: forall m
-   . Monad m
-  => Env.Env m
-  -> ((S.CirclesState -> S.CirclesState) -> m Unit)
-  -> S.CirclesState
-  -> CirclesAction
-  -> m Unit
-circlesControl env = C.mkControl
-  _circlesStateMachine
-  { landing: States.landing $ env
-  , infoGeneral: States.infoGeneral env
-  , askUsername: States.askUsername env
-  , askEmail: States.askEmail env
-  , infoSecurity: States.infoSecurity env
-  , magicWords: States.magicWords env
-  , submit: States.submit env
-  , dashboard: States.dashboard env
-  , login: States.login env
-  , trusts: States.trusts env
-  , debug: States.debug env
-  }
+
+circlesControl ::
+  forall m.
+  Monad m =>
+  Env.Env m ->
+  CirclesConfig ->
+  ((S.CirclesState -> S.CirclesState) -> m Unit) ->
+  S.CirclesState ->
+  CirclesAction ->
+  m Unit
+circlesControl env cfg =
+  C.mkControl
+    _circlesStateMachine
+    { landing: States.landing $ env
+    , infoGeneral: States.infoGeneral env
+    , askUsername: States.askUsername env
+    , askEmail: States.askEmail env
+    , infoSecurity: States.infoSecurity env
+    , magicWords: States.magicWords env
+    , submit: States.submit env
+    , dashboard: States.dashboard env
+    , login: States.login env
+    , trusts: States.trusts env
+    , debug: States.debug env
+    }
--- pkgs/purs/circles-pink-state-machine/src/CirclesPink/Garden/TS.purs
+++ pkgs/purs/circles-pink-state-machine/src/CirclesPink/Garden/TS.purs
@@ -1,8 +1,10 @@
 module CirclesPink.Garden.TS where
 
 import Prelude
+
 import CirclesPink.Garden.Env as Garden
 import CirclesPink.Garden.StateMachine.Action (CirclesAction)
+import CirclesPink.Garden.StateMachine.Config (CirclesConfig)
 import CirclesPink.Garden.StateMachine.Control (circlesControl)
 import CirclesPink.Garden.StateMachine.State (CirclesState)
 import Effect (Effect)
@@ -11,11 +13,11 @@ import Effect.Class (liftEffect)
 import HTTP.Milkis (milkisRequest)
 import Milkis.Impl.Window (windowFetch)
 
-mkControl :: Garden.EnvVars -> ((CirclesState -> CirclesState) -> Effect Unit) -> CirclesState -> CirclesAction -> Effect Unit
-mkControl envVars f s a =
-  (circlesControl env (liftEffect <<< f) s a)
+mkControl :: Garden.EnvVars -> CirclesConfig -> ((CirclesState -> CirclesState) -> Effect Unit) -> CirclesState -> CirclesAction -> Effect Unit
+mkControl envVars cfg f s a =
+  (circlesControl env cfg (liftEffect <<< f) s a)
     # launchAff_
   where
   request = milkisRequest windowFetch
-  env = Garden.env { request, envVars }
 
+  env = Garden.env { request, envVars }
