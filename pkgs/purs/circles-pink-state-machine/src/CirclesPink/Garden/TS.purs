3c089a612aae04e2af0717ad876656936b306469
Michael Bock <mb@thought2.de>
2022-06-16 12:55:28 +0200

    Continue with Config

--- pkgs/purs/circles-pink-state-machine/src/CirclesPink/Garden/TS.purs
+++ pkgs/purs/circles-pink-state-machine/src/CirclesPink/Garden/TS.purs
@@ -4,7 +4,7 @@ import Prelude
 
 import CirclesPink.Garden.Env as Garden
 import CirclesPink.Garden.StateMachine.Action (CirclesAction)
-import CirclesPink.Garden.StateMachine.Config (CirclesConfig)
+import CirclesPink.Garden.StateMachine.Config (CirclesConfig, mapCirclesConfig)
 import CirclesPink.Garden.StateMachine.Control (circlesControl)
 import CirclesPink.Garden.StateMachine.State (CirclesState)
 import Effect (Effect)
@@ -13,9 +13,9 @@ import Effect.Class (liftEffect)
 import HTTP.Milkis (milkisRequest)
 import Milkis.Impl.Window (windowFetch)
 
-mkControl :: Garden.EnvVars -> CirclesConfig -> ((CirclesState -> CirclesState) -> Effect Unit) -> CirclesState -> CirclesAction -> Effect Unit
-mkControl envVars cfg f s a =
-  (circlesControl env cfg (liftEffect <<< f) s a)
+mkControl :: Garden.EnvVars -> CirclesConfig Effect -> ((CirclesState -> CirclesState) -> Effect Unit) -> CirclesState -> CirclesAction -> Effect Unit
+mkControl envVars cfg setState s a =
+  (circlesControl env (mapCirclesConfig liftEffect cfg) (liftEffect <<< setState) s a)
     # launchAff_
   where
   request = milkisRequest windowFetch
--- pkgs/purs/circles-pink-state-machine/test/CirclesPink/Garden/StateMachine/Stories.purs
+++ pkgs/purs/circles-pink-state-machine/test/CirclesPink/Garden/StateMachine/Stories.purs
@@ -6,20 +6,14 @@ import Prelude
 import CirclesPink.Garden.Env (TestEnvM, liftEnv, runTestEnvM, testEnv)
 import CirclesPink.Garden.StateMachine.Control.Env (Env)
 import CirclesPink.Garden.StateMachine.State (CirclesState)
-import CirclesPink.Garden.StateMachine.Stories (execScripT', finalizeAccount, loginUser, signUpUser, trustUser)
+import CirclesPink.Garden.StateMachine.Stories (execScripT', finalizeAccount, loginUser, signUpUser)
 import Control.Monad.State (StateT)
-import Convertable (convert)
-import Data.Map (lookup, toUnfoldable)
 import Data.Maybe (Maybe(..))
-import Data.String (toLower)
-import Data.Tuple (snd)
-import Data.Variant (default, onMatch)
 import Data.Variant.Extra (getLabel)
-import Debug (spy)
-import Debug.Extra (todo)
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
-import Wallet.PrivateKey (addrToString, sampleSafeAddress, sampleMnemonic)
+import Wallet.PrivateKey (sampleMnemonic)
+import CirclesPink.Garden.StateMachine.Config (CirclesConfig(..))
 
 --------------------------------------------------------------------------------
 spec :: Spec Unit
@@ -27,11 +21,13 @@ spec =
   let
     env' :: Env (StateT CirclesState TestEnvM)
     env' = liftEnv testEnv
+
+    cfg = CirclesConfig { extractEmail: Nothing }
   in
     describe "CirclesPink.Garden.StateMachine.Stories" do
       describe "A user can signup" do
         it "ends up in `trusts` state" do
-          ( signUpUser env' { username: "Foo", email: "foo@bar.com" }
+          ( signUpUser env' cfg { username: "Foo", email: "foo@bar.com" }
               # execScripT'
               # runTestEnvM
               # getLabel
@@ -41,8 +37,8 @@ spec =
       describe "A user can finalize the account" do
         it "ends up in `dashboard` state" do
           ( ( do
-                _ <- signUpUser env' { username: "Foo", email: "foo@bar.com" }
-                _ <- finalizeAccount env'
+                _ <- signUpUser env' cfg { username: "Foo", email: "foo@bar.com" }
+                _ <- finalizeAccount env' cfg
                 pure unit
             )
               # execScripT'
@@ -54,7 +50,7 @@ spec =
       describe "A user can not login with invalid mnemonic" do
         it "stays in `login` state" do
           ( ( do
-                _ <- loginUser env' { magicWords: "" }
+                _ <- loginUser env' cfg { magicWords: "" }
                 pure unit
             )
               # execScripT'
@@ -66,7 +62,7 @@ spec =
       describe "A user can not login with unregistered account" do
         it "stays in `login` state" do
           ( ( do
-                _ <- loginUser env' { magicWords: "volcano agree attack fiction firm chunk sweet private average undo pen core plunge choose vendor way liar depth romance enjoy hire rhythm little later" }
+                _ <- loginUser env' cfg { magicWords: "volcano agree attack fiction firm chunk sweet private average undo pen core plunge choose vendor way liar depth romance enjoy hire rhythm little later" }
                 pure unit
             )
               # execScripT'
@@ -78,7 +74,7 @@ spec =
       describe "A user can login" do
         it "ends up in `dashboard` state" do
           ( ( do
-                _ <- loginUser env' { magicWords: show sampleMnemonic }
+                _ <- loginUser env' cfg { magicWords: show sampleMnemonic }
                 pure unit
             )
               # execScripT'
@@ -88,28 +84,28 @@ spec =
             `shouldEqual`
               "dashboard"
 
-      -- describe "A user can trust" do
-      --   it "can trust another user" do
-      --     ( ( do
-      --           _ <- loginUser env' { magicWords: show sampleMnemonic }
-      --           _ <- trustUser env' { safeAddress: addrToString sampleSafeAddress }
-      --           pure unit
-      --       )
-      --         # (\x -> spy "b" x)
-      --         # execScripT'
-      --         # runTestEnvM
-      --         # (\x -> spy "a" x)
-      --         #
-      --           ( default Nothing
-      --               # onMatch
-      --                   { dashboard: \st ->
-      --                       let
-      --                         x = spy "x" (addrToString sampleSafeAddress)
-      --                         y = spy "y" $ (toUnfoldable st.trusts :: Array _)
-      --                       in
-      --                         lookup (convert sampleSafeAddress) st.trusts
-      --                           <#> (const unit)
-      --                   }
-      --           )
-      --     )
-      --       `shouldEqual` (Just unit)
+-- describe "A user can trust" do
+--   it "can trust another user" do
+--     ( ( do
+--           _ <- loginUser env' { magicWords: show sampleMnemonic }
+--           _ <- trustUser env' { safeAddress: addrToString sampleSafeAddress }
+--           pure unit
+--       )
+--         # (\x -> spy "b" x)
+--         # execScripT'
+--         # runTestEnvM
+--         # (\x -> spy "a" x)
+--         #
+--           ( default Nothing
+--               # onMatch
+--                   { dashboard: \st ->
+--                       let
+--                         x = spy "x" (addrToString sampleSafeAddress)
+--                         y = spy "y" $ (toUnfoldable st.trusts :: Array _)
+--                       in
+--                         lookup (convert sampleSafeAddress) st.trusts
+--                           <#> (const unit)
+--                   }
+--           )
+--     )
+--       `shouldEqual` (Just unit)
