de886e034b05025be11f50182e4f24abab15997e
nmauersberg <n.mauersberg@gmail.com>
2022-06-24 10:17:25 +0200

    Fix date in balance

--- pkgs/ts/@circles-pink/web-client/src/onboarding/utils/timeCircles.ts
+++ pkgs/ts/@circles-pink/web-client/src/onboarding/utils/timeCircles.ts
@@ -1,5 +1,4 @@
 import Web3 from 'web3';
-import dayjs from 'dayjs';
 import { crcToTc, tcToCrc } from '@circles/timecircles';
 
 // -----------------------------------------------------------------------------
@@ -10,32 +9,28 @@ type Currency = 'CIRCLES' | 'TIME-CIRCLES' | 'EURO';
 
 type Conversion = 'FROM-TIME-CIRCLES' | 'TO-TIME-CIRCLES';
 
+type TcDate = Date | number;
+
 // -----------------------------------------------------------------------------
 // Circles / Timecircles conversion
 // -----------------------------------------------------------------------------
 
-export const convertTimeCirclesToCircles = (amount: number, date?: string) => {
-  const dateTime = date ? dayjs(date) : dayjs();
+export const convertTimeCirclesToCircles = (amount: number, date?: TcDate) => {
+  const dateTime = date || new Date();
   return mapCircles(amount, dateTime, 'FROM-TIME-CIRCLES');
 };
 
-export const convertCirclesToTimeCircles = (amount: number, date?: string) => {
-  const dateTime = date ? dayjs(date) : dayjs();
+export const convertCirclesToTimeCircles = (amount: number, date?: TcDate) => {
+  const dateTime = date || new Date();
   return mapCircles(amount, dateTime, 'TO-TIME-CIRCLES');
 };
 
-const mapCircles = (
-  amount: number,
-  dateTime: dayjs.Dayjs,
-  type: Conversion
-) => {
-  const transactionDateUnix = dayjs(dateTime).unix();
-
+const mapCircles = (amount: number, date: TcDate, type: Conversion) => {
   switch (type) {
     case 'FROM-TIME-CIRCLES':
-      return tcToCrc(transactionDateUnix, amount);
+      return tcToCrc(date, amount);
     case 'TO-TIME-CIRCLES':
-      return crcToTc(transactionDateUnix, amount);
+      return crcToTc(date, amount);
   }
 };
 
@@ -46,26 +41,26 @@ const mapCircles = (
 export function displayBalance(
   amount: string,
   currency: Currency = 'TIME-CIRCLES',
-  date?: string
+  date?: TcDate
 ) {
-  const dateTime = date ? dayjs(date) : dayjs();
+  const dateTime = date || new Date();
   return mapCurrency(amount, dateTime, currency).toFixed(2);
 }
 
-const mapCurrency = (amount: string, dateTime: dayjs.Dayjs, type: Currency) => {
+const mapCurrency = (amount: string, dateTime: TcDate, type: Currency) => {
   switch (type) {
     case 'CIRCLES':
       return Number.parseFloat(Web3.utils.fromWei(amount, 'ether'));
     case 'TIME-CIRCLES':
       return convertCirclesToTimeCircles(
         Number.parseFloat(Web3.utils.fromWei(amount, 'ether')),
-        dateTime.toString()
+        dateTime
       );
     case 'EURO':
       return (
         convertCirclesToTimeCircles(
           Number.parseFloat(Web3.utils.fromWei(amount, 'ether')),
-          dateTime.toString()
+          dateTime
         ) / 10
       );
   }
