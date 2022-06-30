e766fc8437a6055a604a57784dc65119fdf39999
Michael Bock <mb@thought2.de>
2022-06-30 12:02:34 +0200

    Enhance fp-ts in purs

--- pkgs/purs/circles-pink-state-machine/src/CirclesPink/Garden/StateMachine/State/Dashboard/Views.purs
+++ pkgs/purs/circles-pink-state-machine/src/CirclesPink/Garden/StateMachine/State/Dashboard/Views.purs
@@ -31,6 +31,7 @@ import CirclesPink.Garden.StateMachine.State (DashboardState)
 import CirclesPink.Garden.StateMachine.ViewUtils (nubRemoteReport)
 import Data.Array (any, filter)
 import Data.Either (Either(..))
+import Data.FpTs.Tuple as FPT
 import Data.IxGraph (IxGraph, getIndex)
 import Data.IxGraph as G
 import Data.Maybe (Maybe(..), maybe)
@@ -38,6 +39,7 @@ import Data.Newtype (unwrap)
 import Data.Tuple.Nested (type (/\), (/\))
 import Data.Variant (Variant, default, onMatch)
 import Foreign.Object (Object, values)
+import FpTs.Class (toFpTs)
 import Network.Ethereum.Core.Signatures (Address)
 import RemoteData (RemoteData, isLoading)
 import RemoteReport (RemoteReport)
@@ -68,6 +70,10 @@ globalLoading d = any (_ == true) $ join checks
 type DefaultView =
   { trustsConfirmed :: Trusts
   , trustsCandidates :: Trusts
+  , graph :: Array (Address FPT./\ UserIdent)
+      -- { nodes :: Array (Address FPT./\ UserIdent)
+      -- , edges :: Array { from :: Address, to :: Address, data :: UserIdent }
+      -- }
   , usersSearch :: Trusts
   , userSearchResult :: RemoteReport ErrUserSearchResolved (Array User)
   , getUsersResult :: RemoteData_ ErrGetUsersResolved (Array User)
@@ -129,6 +135,7 @@ defaultView d@{ trusts } =
   in
     { trustsConfirmed: d.trusts # G.outgoingEdgesWithNodes d.user.safeAddress # maybe [] identity # filter (\(e /\ _) -> isTrusted e) <#> (mapTrust' d.user.safeAddress d.trusts)
     , trustsCandidates: d.trusts # G.outgoingEdgesWithNodes d.user.safeAddress # maybe [] identity # filter (\(e /\ _) -> not $ isTrusted e) <#> (mapTrust' d.user.safeAddress d.trusts)
+    , graph: d.trusts # G.toUnfoldables # (_.nodes :: _ -> Array _) # toFpTs   -- { nodes: [], edges: [] }
     , usersSearch: usersSearch
     , userSearchResult: d.userSearchResult
     , getUsersResult: d.getUsersResult
