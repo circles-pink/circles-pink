5f5c84c3bdadc3b240da9cf3882411402df44403
nmauersberg <n.mauersberg@gmail.com>
2022-06-30 16:33:21 +0200

    Records fpTs

--- pkgs/purs/fp-ts/src/FpTs/Class.purs
+++ pkgs/purs/fp-ts/src/FpTs/Class.purs
@@ -2,8 +2,12 @@ module FpTs.Class where
 
 import Prelude
 
+import Debug.Extra (todo)
+import Prim.RowList (class RowToList, RowList)
+import Type.Proxy (Proxy(..))
+
 class FpTs a b where
-  toFpTs ::  a -> b
+  toFpTs :: a -> b
   fromFpTs :: b -> a
 
 instance fpTsInt :: FpTs Int Int where
@@ -27,6 +31,25 @@ instance fpTsFunction :: FpTs (Function a b) (Function a b) where
   fromFpTs = identity
 
 instance fpTsArray :: FpTs a b => FpTs (Array a) (Array b) where
-  toFpTs = map toFpTs 
+  toFpTs = map toFpTs
   fromFpTs = map fromFpTs
 
+instance fpTsRecord ::
+  ( RowToList r rl
+  , GenericRecordToFpTs rl r'
+  , RowToList r' rl'
+  , GenericRecordFromFpTs rl' r
+  ) =>
+  FpTs (Record r) (Record r') where
+  toFpTs _ = genRecordToFpTs (Proxy :: _ rl)
+  fromFpTs _ = genRecordFromFpTs (Proxy :: _ rl')
+
+class GenericRecordToFpTs :: RowList Type -> Row Type -> Constraint
+class GenericRecordToFpTs rl r where
+  genRecordToFpTs :: (Proxy rl) -> Record r
+
+-- instance genericRecordToFpTsNil :: FpTs (Nil r) ->
+
+class GenericRecordFromFpTs :: RowList Type -> Row Type -> Constraint
+class GenericRecordFromFpTs rl r where
+  genRecordFromFpTs :: (Proxy rl) -> Record r
