bcd11d0f1676bea5d7af98fb19cf3e68770ef75d
Michael Bock <me@thought2.de>
2022-07-01 09:20:27 +0200

    Write gneric record instance for FpTs - uses heterogeneous package

--- pkgs/purs/fp-ts/src/FpTs/Class.purs
+++ pkgs/purs/fp-ts/src/FpTs/Class.purs
@@ -2,12 +2,7 @@ module FpTs.Class where
 
 import Prelude
 
-import Data.Symbol (class IsSymbol)
-import Debug.Extra (todo)
-import Prim.Row (class Cons)
-import Prim.RowList (class RowToList, Cons, Nil, RowList)
-import Type.Proxy (Proxy(..))
-import Record as R
+import Heterogeneous.Mapping (class HMap, class Mapping, hmap)
 
 class FpTs a b where
   toFpTs :: a -> b
@@ -37,34 +32,28 @@ instance fpTsArray :: FpTs a b => FpTs (Array a) (Array b) where
   toFpTs = map toFpTs
   fromFpTs = map fromFpTs
 
--- instance fpTsRecord ::
---   ( RowToList r rl
---   , GenericRecordToFpTs rl () r'
---   , RowToList r' rl'
---   , GenericRecordFromFpTs rl' r
---   ) =>
---   FpTs (Record r) (Record r') where
---   toFpTs _ = genRecordToFpTs (Proxy :: _ rl) {}
---   fromFpTs _ = genRecordFromFpTs (Proxy :: _ rl')
--- --------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
--- class GenericRecordToFpTs :: RowList Type -> Row Type -> Row Type -> Constraint
--- class GenericRecordToFpTs rl r1 r2 | rl -> r1 r2 where
---   genRecordToFpTs :: (Proxy rl) -> Record r1 -> Record r2
+data FromFpTs = FromFpTs
 
--- instance genericRecordToFpTsNil :: GenericRecordToFpTs Nil () () where
---   genRecordToFpTs _ r = r 
+instance fromFpTsInst ::
+  ( FpTs b a
+  ) =>
+  Mapping FromFpTs a b where
+  mapping FromFpTs = fromFpTs
 
--- instance genericRecordToFpTsCons ::
---   ( IsSymbol s
---   , GenericRecordToFpTs rl r1 r2
---   --, Cons s a r' r
---   ) =>
---   GenericRecordToFpTs (Cons s a rl) r1 r2 where
---   genRecordToFpTs _ r = todo --R.set (Proxy :: _ s) 
+data ToFpTs = ToFpTs
 
--- --------------------------------------------------------------------------------
+instance toFpTsInst ::
+  ( FpTs a b
+  ) =>
+  Mapping ToFpTs a b where
+  mapping ToFpTs = toFpTs
 
--- class GenericRecordFromFpTs :: RowList Type -> Row Type -> Constraint
--- class GenericRecordFromFpTs rl r where
---   genRecordFromFpTs :: (Proxy rl) -> Record r
+instance fpTsRecord ::
+  ( HMap ToFpTs (Record r) (Record r')
+  , HMap FromFpTs (Record r') (Record r)
+  ) =>
+  FpTs (Record r) (Record r') where
+  toFpTs = hmap ToFpTs
+  fromFpTs = hmap FromFpTs
