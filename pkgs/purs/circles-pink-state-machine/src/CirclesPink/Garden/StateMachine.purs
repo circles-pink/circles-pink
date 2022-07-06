e5c50bb973a670b0e7846d6fd198e3deb3b9e05c
Michael Bock <me@thought2.de>
2022-07-06 06:58:03 +0200

    Fix TS

--- pkgs/purs/circles-pink-state-machine/src/CirclesPink/Garden/StateMachine.purs
+++ pkgs/purs/circles-pink-state-machine/src/CirclesPink/Garden/StateMachine.purs
@@ -1,23 +1,10 @@
 module CirclesPink.Garden.StateMachine
-  ( CirclesStateMachine
-  , check
-  , _circlesStateMachine
+  ( module Exp
   ) where
 
-import Prelude
-import CirclesPink.Garden.StateMachine.Action as A
-import CirclesPink.Garden.StateMachine.Protocol as P
-import CirclesPink.Garden.StateMachine.State as S
-import Stadium.Type.StateMachine as STM
-import Type.Proxy (Proxy(..))
 
-type CirclesStateMachine = STM.StateMachine
-  P.CirclesProtocol
-  S.CirclesState
-  A.CirclesAction
-
-_circlesStateMachine :: Proxy CirclesStateMachine
-_circlesStateMachine = Proxy :: _ CirclesStateMachine
-
-check :: Unit
-check = STM.validate (Proxy :: _ CirclesStateMachine)
+import CirclesPink.Garden.StateMachine.Action (CirclesAction, _addTrustConnection, _askEmail, _askUsername, _checkForSession, _checkUBIPayout, _coreToWindow, _dashboard, _debug, _finalizeRegisterUser, _getBalance, _getSafeStatus, _getTrusts, _getUsers, _infoGeneral, _infoSecurity, _landing, _login, _magicWords, _newPrivKey, _next, _prev, _redeploySafeAndToken, _removeTrustConnection, _requestUBIPayout, _setEmail, _setMagicWords, _setPrivacy, _setTerms, _setUsername, _signIn, _signUp, _submit, _transfer, _trusts, _userSearch) as Exp
+import CirclesPink.Garden.StateMachine.State (AskEmailState, AskUsernameState, CirclesState, DashboardState, DebugState, EmailApiResult, ErrGetUsers, ErrLandingState, ErrLandingStateResolved, ErrLoginState, ErrSubmit, ErrSubmitResolved, ErrTokenCheckUBIPayout, ErrTokenGetBalance, ErrTokenRequestUBIPayout, ErrTokenTransfer, ErrTrustAddConnection, ErrTrustGetTrusts, ErrTrustState, ErrUserSearch, GetUsersResult, InfoGeneralState, InfoSecurityState, InitDashboard, LandingState, LandingStateCheckSessionResult, LoginState, MagicWordsState, SubmitState, TokenCheckUBIPayoutResult, TokenGetBalanceResult, TokenRequestUBIPayoutResult, TokenTransferResult, TrustAddResult, TrustGetTrusts, TrustState, TrustsDeploySafeResult, TrustsDeployTokenResult, UserData, UserDataSubmitResult, UsernameApiResult, init, initDashboard, initDebug, initLanding, initLogin) as Exp
+import CirclesPink.Garden.StateMachine.Config (CirclesConfig(..), mapCirclesConfig) as Exp
+import CirclesPink.Garden.StateMachine.Control (circlesControl) as Exp
+import CirclesPink.Garden.StateMachine.Type (CirclesStateMachine, _circlesStateMachine, check) as Exp
--- pkgs/purs/circles-pink-state-machine/src/CirclesPink/Garden/TS.purs
+++ pkgs/purs/circles-pink-state-machine/src/CirclesPink/Garden/TS.purs
@@ -3,23 +3,16 @@ module CirclesPink.Garden.TS
   , convertConfig
   , mkControl
   , mkControlTestEnv
-  )
-  where
+  ) where
 
-import Prelude
+import CirclesPink.Prelude
 
 import CirclesPink.Garden.Env as Garden
-import CirclesPink.Garden.StateMachine.Action (CirclesAction)
+import CirclesPink.Garden.StateMachine (CirclesAction, circlesControl, CirclesState, initLanding)
 import CirclesPink.Garden.StateMachine.Config as C
-import CirclesPink.Garden.StateMachine.Control (circlesControl)
 import CirclesPink.Garden.StateMachine.Control.Class.TestScriptT (evalTestScriptT)
-import CirclesPink.Garden.StateMachine.State (CirclesState, initLanding)
 import CirclesPink.Garden.TestEnv (testEnv)
-import Data.Either (Either(..))
 import Data.FpTs.Either as FP
-import Effect (Effect)
-import Effect.Aff (launchAff_)
-import Effect.Class (class MonadEffect, liftEffect)
 import FpTs.Class (fromFpTs)
 import HTTP.Milkis (milkisRequest)
 import Milkis.Impl.Window (windowFetch)
@@ -35,20 +28,16 @@ convertConfig cfg = C.CirclesConfig
 
 mkControl :: Garden.EnvVars -> CirclesConfig -> ((CirclesState -> CirclesState) -> Effect Unit) -> CirclesState -> CirclesAction -> Effect Unit
 mkControl envVars cfg setState s a =
-  (circlesControl env (C.mapCirclesConfig liftEffect (convertConfig cfg)) (liftEffect <<< setState) s a)
+  circlesControl env cfg' (liftEffect <<< setState) s a
     # launchAff_
   where
+  cfg' = C.mapCirclesConfig liftEffect $ convertConfig cfg
   request = milkisRequest windowFetch
-
   env = Garden.env { request, envVars }
 
 mkControlTestEnv :: ((CirclesState -> CirclesState) -> Effect Unit) -> CirclesState -> CirclesAction -> Effect Unit
-mkControlTestEnv setState st ac = 
+mkControlTestEnv setState st ac =
   circlesControl testEnv cfg (setState >>> liftEffect) st ac
     # evalTestScriptT initLanding
   where
-  cfg = C.CirclesConfig
-    { extractEmail:
-        --Left "my@email.com"
-        Right (\_ -> pure unit)
-    }
+  cfg = C.CirclesConfig { extractEmail: Right (\_ -> pure unit) }
