54d82e57a18a5911f05645586816fd14c8b9051d
Michael Bock <mb@thought2.de>
2022-07-07 16:49:54 +0200

    Finish graph refactoring

--- pkgs/purs/graph/test/Data/Graph/Core.purs
+++ pkgs/purs/graph/test/Data/Graph/Core.purs
@@ -18,67 +18,7 @@ type Graph_ = Graph String Unit Unit
 spec :: Spec Unit
 spec =
   describe "Data.Graph.Core" do
-    -- describe "empty"
-    --   let
-    --     graph :: Graph_
-    --     graph = C.empty
-    --   in
-    --     it "retrieves no node ids" do
-    --       (graph # G.toUnfoldables) `shouldEqual` { nodes: [], edges: [] }
-    -- describe "outgoingIds"
-    --   let
-    --     graph = C.empty
-    --       # C.insertNode "n1" unit
-    --       # C.insertNode "n2" unit
-    --       # C.insertEdge "n1" "n2" unit
-    --   in
-    --     it "retrieves all outgoing ids at a given id" do
-    --       (C.outgoingIds "n1" graph) `shouldEqual`
-    --         { nodes:
-    --             [ "n1" /\ unit
-    --             , "n2" /\ unit
-    --             ]
-    --         , edges:
-    --             [ "n1" /\ "n2" /\ unit
-    --             , "n1" /\ "n2" /\ unit
-    --             ]
-    --         }
-    -- describe "incomingIds"
-    --   let
-    --     graph = C.empty
-    --       # C.insertNode "n1" unit
-    --       # C.insertNode "n2" unit
-    --       # C.insertEdge "n1" "n2" unit
-    --   in
-    --     it "retrieves all imcoming ids at a given id" do
-    --       (C.incomingIds "n2" graph) `shouldEqual` (Just $ S.fromFoldable [ "n1" ])
-    -- describe "nodeIds"
-    --   let
-    --     graph = C.empty
-    --       # C.insertNode "n1" unit
-    --       # C.insertNode "n2" unit
-    --   in
-    --     it "retrieves all node ids in the graph" do
-    --       (C.nodeIds graph) `shouldEqual` (S.fromFoldable [ "n1", "n2" ])
-    -- describe "lookupNode"
-    --   let
-    --     graph = C.empty
-    --       # C.insertNode "n1" 1
-    --       # C.insertNode "n2" 2
-    --   in
-    --     it "retrieves the node at the given id" do
-    --       (C.lookupNode "n1" graph) `shouldEqual` (Just 1)
-    -- describe "lookupEdge"
-    --   let
-    --     graph = C.empty
-    --       # C.insertNode "n1" 1
-    --       # C.insertNode "n2" 2
-    --       # C.insertEdge "n1" "n2" 11
-    --   in
-    --     it "retrieves the edge at from the given source and target ids" do
-    --       (C.lookupEdge "n1" "n2" graph) `shouldEqual` (Just 11)
-    describe
-      "insertNode"
+    describe "insertNode"
       let
         graph :: Maybe (Graph String Int Int)
         graph = fromFoldables
@@ -93,8 +33,32 @@ spec =
                 , edges: []
                 }
             )
-    describe
-      "deleteEdge"
+    describe "attemptDeleteNode"
+      let
+        graph :: Maybe (Graph String Int Int)
+        graph = fromFoldables
+          { nodes: [ "n1" /\ 1 ]
+          , edges: []
+          }
+      in
+        it "..." do
+          (graph <#> C.attemptDeleteNode "n1" <#> toUnfoldables) `shouldEqual`
+            ( Just
+                { nodes: []
+                , edges: []
+                }
+            )
+    describe "lookupNode"
+      let
+        graph :: Maybe (Graph String Int Int)
+        graph = fromFoldables
+          { nodes: [ "n1" /\ 1 ]
+          , edges: []
+          }
+      in
+        it "..." do
+          (graph >>= C.lookupNode "n1") `shouldEqual` (Just 1)
+    describe "deleteEdge"
       let
         graph =
           fromFoldables
@@ -102,10 +66,50 @@ spec =
             , edges: [ ("n1" ~ "n2") /\ 11 ]
             }
       in
-        it "retrieves the edge at from the given source and target ids" do
+        it "..." do
           (graph <#> C.attemptDeleteEdge ("n1" ~ "n2") <#> toUnfoldables) `shouldEqual`
             ( Just
                 { nodes: [ "n1" /\ 1, "n2" /\ 2 ]
                 , edges: []
+                }
+            )
+    describe "lookupEdge"
+      let
+        graph =
+          fromFoldables
+            { nodes: [ "n1" /\ 1, "n2" /\ 2 ]
+            , edges: [ ("n1" ~ "n2") /\ 11 ]
+            }
+      in
+        it "..." do
+          (graph >>= C.lookupEdge ("n1" ~ "n2")) `shouldEqual` (Just 11)
+    describe "attemptDeleteEdge"
+      let
+        graph =
+          fromFoldables
+            { nodes: [ "n1" /\ 1, "n2" /\ 2 ]
+            , edges: [ ("n1" ~ "n2") /\ 11 ]
+            }
+      in
+        it "..." do
+          (graph <#> C.attemptDeleteEdge ("n1" ~ "n2") <#> toUnfoldables) `shouldEqual`
+            ( Just
+                { nodes: [ "n1" /\ 1, "n2" /\ 2 ]
+                , edges: []
+                }
+            )
+    describe "insertEdge"
+      let
+        graph =
+          fromFoldables
+            { nodes: [ "n1" /\ 1, "n2" /\ 2 ]
+            , edges: []
+            }
+      in
+        it "..." do
+          (graph >>= C.insertEdge ("n1" ~ "n2") 11 <#> toUnfoldables) `shouldEqual`
+            ( Just
+                { nodes: [ "n1" /\ 1, "n2" /\ 2 ]
+                , edges: [ ("n1" ~ "n2") /\ 11 ]
                 }
             )
