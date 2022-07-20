05adc0a106b5344ccfa900942de54e52d39fb86e
Michael Bock <mb@thought2.de>
2022-07-20 14:31:26 +0200

    Implement String Storage

--- pkgs/purs/circles-pink-state-machine/src/string-storage/src/StringStorage.purs
+++ pkgs/purs/circles-pink-state-machine/src/string-storage/src/StringStorage.purs
@@ -3,10 +3,16 @@ module StringStorage where
 import Prelude
 
 import Control.Monad.Except (ExceptT)
+import Control.Monad.Maybe.Trans (runMaybeT)
+import Data.Either (hush)
 import Data.Maybe (Maybe)
 import Debug.Extra (todo)
 import Effect (Effect)
 import Effect.Aff (Aff)
+import Effect.Exception (try)
+import Web.HTML (window)
+import Web.HTML.Window (localStorage)
+import Web.Storage.Storage (clear, getItem, removeItem, setItem)
 
 type StringStorage =
   { setItem :: String -> String -> Aff Unit
@@ -16,7 +22,15 @@ type StringStorage =
   }
 
 getLocalStorage :: Effect (Maybe StringStorage)
-getLocalStorage = todo
+getLocalStorage = runMaybeT do
+  w <- try window # hush
+  s <- localStorage w
+  pure
+    { setItem: \k v -> setItem k v s
+    , getItem: \k -> getItem k s
+    , clear: clear s
+    , removeItem: \k -> removeItem k s
+    }
 
 getSessionStorage :: Effect (Maybe StringStorage)
 getSessionStorage = todo
