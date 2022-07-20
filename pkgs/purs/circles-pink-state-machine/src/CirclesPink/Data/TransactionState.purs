471416594bbe96aecf0994cd41fdfe755d6629b0
nmauersberg <n.mauersberg@gmail.com>
2022-07-21 00:22:24 +0200

    Increase cise layout edge length & fix more warnings

--- pkgs/purs/circles-pink-state-machine/src/CirclesPink/Data/TransactionState.purs
+++ pkgs/purs/circles-pink-state-machine/src/CirclesPink/Data/TransactionState.purs
@@ -1,45 +1,42 @@
-module CirclesPink.Data.TxState
-  ( TxSeqState
-  ) where
+module CirclesPink.Data.TxState where
 
-import CirclesPink.Prelude
+-- import CirclesPink.Prelude
 
-import CirclesPink.Data.Address (Address)
-import Data.Array.NonEmpty (NonEmptyArray)
-import Data.BN (BN)
-import Debug.Extra (todo)
-import RemoteReport (RemoteReport)
+-- import CirclesPink.Data.Address (Address)
+-- import Data.Array.NonEmpty (NonEmptyArray)
+-- import Data.BN (BN)
+-- import Debug.Extra (todo)
+-- import RemoteReport (RemoteReport)
 
-type EitherV r a = Either (Variant r) a
+-- type EitherV r a = Either (Variant r) a
 
-newtype TxHash = TxHash String
+-- newtype TxHash = TxHash String
 
-newtype TxHop = TxHop { from :: Address, to :: Address, value :: BN, index :: Int }
+-- newtype TxHop = TxHop { from :: Address, to :: Address, value :: BN, index :: Int }
 
-data SucceededTxHop = SucceededTxHop TxHash TxHop
+-- data SucceededTxHop = SucceededTxHop TxHash TxHop
 
+-- data TxSeqState
+--   = NoTxSeq
+--   | PrepareTxSeq (NonEmptyArray TxHop)
+--   | RunningTxSeq (NonEmptyArray TxHop) (RemoteReport (Variant (Err ())) TxHash)
 
+-- type TxHopRemoteReport = RemoteReport (Variant (Err ())) TxHash
 
-data TxSeqState
-  = NoTxSeq
-  | PrepareTxSeq (NonEmptyArray TxHop)
-  | RunningTxSeq (NonEmptyArray TxHop) (RemoteReport (Variant (Err ())) TxHash)
-type TxHopRemoteReport = RemoteReport (Variant (Err ())) TxHash
+-- init :: forall r. TxSeqState -> EitherV (Err r) TxSeqState
+-- init = todo
 
-init :: forall r. TxSeqState -> EitherV (Err r) TxSeqState
-init = todo
+-- prepareTxSeq :: forall r. NonEmptyArray TxHop -> TxSeqState -> EitherV (Err r) TxSeqState
+-- prepareTxSeq = todo
 
-prepareTxSeq :: forall r. NonEmptyArray TxHop -> TxSeqState -> EitherV (Err r) TxSeqState
-prepareTxSeq = todo
+-- startTxSeq :: forall r. TxSeqState -> EitherV (Err r) TxSeqState
+-- startTxSeq = todo
 
-startTxSeq :: forall r. TxSeqState -> EitherV (Err r) TxSeqState
-startTxSeq = todo
+-- updateTxHop :: forall r. TxHopRemoteReport -> TxSeqState -> EitherV (Err r) TxSeqState
+-- updateTxHop = todo
 
-updateTxHop :: forall r. TxHopRemoteReport -> TxSeqState -> EitherV (Err r) TxSeqState
-updateTxHop = todo
+-- failTxSeq :: forall r. TxSeqState -> EitherV (Err r) TxSeqState
+-- failTxSeq = todo
 
-failTxSeq :: forall r. TxSeqState -> EitherV (Err r) TxSeqState
-failTxSeq = todo
-
-type Err :: forall k. k -> k
-type Err r = r
+-- type Err :: forall k. k -> k
+-- type Err r = r
