5fa06fd248238afba47055fdb6e066afd0e40222
Michael Bock <mb@thought2.de>
2022-07-20 14:46:29 +0200

    Refactor String Storage

--- pkgs/purs/circles-pink-state-machine/src/string-storage/src/StringStorage.purs
+++ pkgs/purs/circles-pink-state-machine/src/string-storage/src/StringStorage.purs
@@ -2,13 +2,14 @@ module StringStorage where
 
 import Prelude
 
-import Control.Monad.Except (ExceptT)
-import Control.Monad.Maybe.Trans (runMaybeT)
-import Data.Either (hush)
+import Control.Monad.Except (ExceptT(..), lift)
+import Control.Monad.Maybe.Trans (MaybeT(..), runMaybeT)
+import Data.Either (hush, note)
 import Data.Maybe (Maybe)
 import Debug.Extra (todo)
 import Effect (Effect)
 import Effect.Aff (Aff)
+import Effect.Class (liftEffect)
 import Effect.Exception (try)
 import Web.HTML (window)
 import Web.HTML.Window (localStorage)
@@ -16,20 +17,20 @@ import Web.Storage.Storage (clear, getItem, removeItem, setItem)
 
 type StringStorage =
   { setItem :: String -> String -> Aff Unit
-  , getItem :: String -> ExceptT Unit Aff String
-  , deleteItem :: String -> ExceptT Unit Aff Unit
+  , getItem :: String -> Aff (Maybe String)
+  , deleteItem :: String -> Aff Unit
   , clear :: Aff Unit
   }
 
 getLocalStorage :: Effect (Maybe StringStorage)
 getLocalStorage = runMaybeT do
-  w <- try window # hush
-  s <- localStorage w
+  w <- try window <#> hush # MaybeT
+  s <- localStorage w # liftEffect
   pure
-    { setItem: \k v -> setItem k v s
-    , getItem: \k -> getItem k s
-    , clear: clear s
-    , removeItem: \k -> removeItem k s
+    { setItem: \k v -> liftEffect $ setItem k v s
+    , getItem: \k -> todo -- ExceptT $ liftEffect $ note unit <$> getItem k s
+    , clear: todo -- liftEffect $ clear s
+    , deleteItem: todo -- \k -> liftEffect $ removeItem k s
     }
 
 getSessionStorage :: Effect (Maybe StringStorage)
