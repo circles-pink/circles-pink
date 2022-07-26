bdc839d16582bc6e3c56fc094f4d7fc7d94cbcf5
Michael Bock <me@thought2.de>
2022-07-26 22:30:52 +0200

    Handle typeclasses

--- pkgs/ts/@circles-pink/web-client/src/components/TrustUserList.tsx
+++ pkgs/ts/@circles-pink/web-client/src/components/TrustUserList.tsx
@@ -1,12 +1,17 @@
+import { Address } from '@circles-pink/state-machine/output/CirclesPink.Data.Address';
 import { CirclesGraph } from '@circles-pink/state-machine/output/CirclesPink.Garden.StateMachine.State.Dashboard';
+import { neighborNodes } from '@circles-pink/state-machine/output/Data.IxGraph';
 import React from 'react';
 
 type Props = {
-    graph : CirclesGraph
+    graph : CirclesGraph,
+    address: Address
 }
 
 
 export const TrustUserList = (props: Props) => {
+
+    const neighbors = neighborNodes()
    
     return <div>Hello! tul</div>
 };
