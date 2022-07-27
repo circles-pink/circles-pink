5963b49a7b7b1e40ba5c5f6e7d6254eb57d60851
Michael Bock <me@thought2.de>
2022-07-27 07:55:12 +0200

    Use nullable

--- pkgs/ts/@circles-pink/web-client/src/components/TrustUserList.tsx
+++ pkgs/ts/@circles-pink/web-client/src/components/TrustUserList.tsx
@@ -1,9 +1,14 @@
 import { Address, ordAddress } from '@circles-pink/state-machine/output/CirclesPink.Data.Address';
 import { CirclesGraph } from '@circles-pink/state-machine/output/CirclesPink.Garden.StateMachine.State.Dashboard';
 import { ordTrustNode } from '@circles-pink/state-machine/output/CirclesPink.Data.TrustNode';
+import { toNullable } from '@circles-pink/state-machine/output/Data.Nullable';
+
 
 import { neighborNodes } from '@circles-pink/state-machine/output/Data.IxGraph';
 import React from 'react';
+import { either, hush } from '@circles-pink/state-machine/output/Data.Either';
+import { TrustNode } from '@circles-pink/state-machine/output/CirclesCore';
+import { pipe } from 'fp-ts/lib/function';
 
 type Props = {
     graph : CirclesGraph,
@@ -14,6 +19,12 @@ type Props = {
 export const TrustUserList = ({address, graph}: Props) => {
 
     const neighbors = neighborNodes(ordAddress)(address)(graph)
+
+    const trustNodes = pipe(neighbors, hush, toNullable)
+
+    if (!trustNodes) return <div>Address not found in graph</div>
    
+    const x = trustNodes
+
     return <div>Hello! tul</div>
 };
