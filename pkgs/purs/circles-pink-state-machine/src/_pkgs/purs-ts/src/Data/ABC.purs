d040e878f6ddaf657530a292a05cbe3f767a217a
Michael Bock <mb@thought2.de>
2022-07-29 17:09:43 +0200

    Fix merge conflicts

--- pkgs/purs/circles-pink-state-machine/src/_pkgs/purs-ts/src/Data/ABC.purs
+++ pkgs/purs/circles-pink-state-machine/src/_pkgs/purs-ts/src/Data/ABC.purs
@@ -1,6 +1,6 @@
 module Data.ABC where
 
-import CirclesPink.GenerateTSD.Class (class ToTsType)
+
 
 data A = A
 
@@ -53,24 +53,3 @@ data X
 data Y
 
 data Z
-
-
-instance toTsTypeA :: ToTsType A where
-  toTsType _ = DTS.TypeVar $ DTS.Name "A"
-
-instance toTsTypeB :: ToTsType B where
-  toTsType _ = DTS.TypeVar $ DTS.Name "B"
-
-instance toTsTypeC :: ToTsType C where
-  toTsType _ = DTS.TypeVar $ DTS.Name "C"
-
-instance toTsTypeD :: ToTsType D where
-  toTsType _ = DTS.TypeVar $ DTS.Name "D"
-
-instance toTsTypeE :: ToTsType E where
-  toTsType _ = DTS.TypeVar $ DTS.Name "E"
-
-
-instance toTsTypeZ :: ToTsType Z where
-  toTsType _ = DTS.TypeVar $ DTS.Name "Z"
-
