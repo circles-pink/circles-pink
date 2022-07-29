4cc7dcbb93b5b796d77564312bafc12dbb6ea00a
Michael Bock <me@thought2.de>
2022-07-29 07:08:42 +0200

    Add DTS DSL

--- pkgs/purs/circles-pink-state-machine/test/Main.purs
+++ pkgs/purs/circles-pink-state-machine/test/Main.purs
@@ -3,18 +3,21 @@ module Test.CirclesPinkStateMachine.Main where
 import Prelude
 
 import CirclesCore.Tests as CirclesCore.Tests
+import CirclesPink.Garden.StateMachine.Control.States.Dashboard as CirclesPink.Garden.StateMachine.Control.States.Dashboard
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import GunDB.Tests as GunDB.Tests
 import PursDeps.Tests as PursDeps.Tests
 import Test.CirclesPink.Garden.StateMachine.Control.States.Dashboard as Test.CirclesPink.Garden.StateMachine.Control.States.Dashboard
 import Test.Data.BigInt as Test.Data.BigInt
+import Test.Spec.Discovery (discover)
 import Test.Spec.Reporter (consoleReporter)
 import Test.Spec.Runner (runSpec)
 import Test.Unit.Main (runTest)
 import Test.Wallet.PrivateKey as Test.Wallet.PrivateKey
 import CirclesPink.Garden.StateMachine.Control.States.Dashboard as CirclesPink.Garden.StateMachine.Control.States.Dashboard
 import Test.VoucherServer.Main as Test.VoucherServer.Main
+
 foreign import globalRequires :: {}
 
 mainTestUnit :: Effect Unit
@@ -24,15 +27,18 @@ mainTestUnit =
     Test.Data.BigInt.tests
     PursDeps.Tests.tests
 
+-- mainTestSpec :: Effect Unit
+-- mainTestSpec =
+--   launchAff_
+--     $ runSpec [ consoleReporter ] do
+--         CirclesCore.Tests.spec
+--         GunDB.Tests.spec
+--         Test.CirclesPink.Garden.StateMachine.Control.States.Dashboard.spec
+--         CirclesPink.Garden.StateMachine.Control.States.Dashboard.spec
 mainTestSpec :: Effect Unit
-mainTestSpec =
-  launchAff_
-    $ runSpec [ consoleReporter ] do
-        CirclesCore.Tests.spec
-        GunDB.Tests.spec
-        Test.CirclesPink.Garden.StateMachine.Control.States.Dashboard.spec
-        CirclesPink.Garden.StateMachine.Control.States.Dashboard.spec
-
+mainTestSpec = launchAff_ do
+  specs <- discover """.*"""
+  runSpec [consoleReporter] specs
 
 main :: Effect Unit
 main = do
