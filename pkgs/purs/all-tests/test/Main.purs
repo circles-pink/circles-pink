ae32b928e7b212dc9bb3942efc5c3764d254d794
Michael Bock <mb@thought2.de>
2022-08-02 16:26:17 +0200

    Fix test discovery

--- pkgs/purs/all-tests/test/Main.purs
+++ pkgs/purs/all-tests/test/Main.purs
@@ -9,5 +9,5 @@ import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
 main = launchAff_ do
-  specs <- discover """.*"""
+  specs <- discover """^(?!(Milkis.Impl.Window|cache-db\.json|Web3\.Bindings|Web3|Web.XHR.XMLHttpRequestUpload|Web.XHR|Web.Storage|Web.Internal.FFI|Web|VoucherServer.Main)).*"""
   runSpec [consoleReporter] specs
