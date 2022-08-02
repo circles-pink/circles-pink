b952b491b7ebc2c3b65acc9a671311d59574992e
Michael Bock <mb@thought2.de>
2022-08-02 11:14:40 +0200

    Add Wrapper and Replace module

---
+++ pkgs/purs/circles-pink-state-machine/src/CirclesPink/GenerateTSD/Replace.purs
@@ -0,0 +1,54 @@
+module CirclesPink.GenerateTSD.Replace where
+
+
+import Data.Either (Either)
+import Data.IxGraph as Data.IxGraph
+import Data.Tuple (Tuple)
+import PursTsGen (class ToTsDef)
+import PursTsGen as PT
+import PursTsGen.Class.ToTsType (class ToTsType)
+import PursTsGen.Lang.TypeScript.DSL as TS
+import Type.Proxy (Proxy)
+import Unsafe.Coerce (unsafeCoerce)
+import CirclesPink.GenerateTSD.Wrappers as W
+
+
+--------------------------------------------------------------------------------
+
+unsafeReplace :: forall a b. UnsafeReplace a b => a -> b
+unsafeReplace = unsafeCoerce
+
+class UnsafeReplace :: forall k1 k2. k1 -> k2 -> Constraint
+class UnsafeReplace a b | a -> b
+
+instance replaceIxGraph ::
+  ( UnsafeReplace a a'
+  , UnsafeReplace b b'
+  , UnsafeReplace c c'
+  ) =>
+  UnsafeReplace (Data.IxGraph.IxGraph a b c) (W.IxGraph a' b' c')
+
+else instance replaceNeighborConnectivity ::
+  ( UnsafeReplace a a'
+  ) =>
+  UnsafeReplace (Data.IxGraph.NeighborConnectivity a) (W.NeighborConnectivity a')
+
+else instance replaceFn :: (UnsafeReplace a a', UnsafeReplace b b') => UnsafeReplace (Function a b) (Function a' b')
+
+else instance replaceTuple :: (UnsafeReplace a a', UnsafeReplace b b') => UnsafeReplace (Tuple a b) (Tuple a' b')
+
+else instance replaceEither :: (UnsafeReplace a a', UnsafeReplace b b') => UnsafeReplace (Either a b) (Either a' b')
+
+else instance replaceArray :: (UnsafeReplace a a') => UnsafeReplace (Array a) (Array a')
+
+else instance replaceProxy :: (UnsafeReplace a a') => UnsafeReplace (Proxy a) (Proxy a')
+
+else instance replace :: UnsafeReplace a a
+
+--------------------------------------------------------------------------------
+
+typeDef :: forall t154 t155. ToTsDef t154 => UnsafeReplace t155 (Proxy t154) => String -> t155 -> Array TS.Declaration
+typeDef s x = PT.typeDef s (unsafeReplace x)
+
+value :: forall t88 t89. ToTsType t88 => UnsafeReplace t89 t88 => String -> Array TS.Type -> t89 -> Array TS.Declaration
+value s xs x = PT.value s xs (unsafeReplace x)
