ad3f0723c217508dd6e10f76663b53635b2002ba
Michael Bock <mb@thought2.de>
2022-08-02 16:26:17 +0200

    Fix test discovery

--- pkgs/purs/all-tests/test/Main.purs
+++ pkgs/purs/all-tests/test/Main.purs
@@ -6,6 +6,6 @@ import Effect (Effect)
 import Test.VoucherServer.Main as Test.VoucherServer.Main
 
 main :: Effect Unit
-main = do
-  Test.VoucherServer.Main.main
-  --runSpec [consoleReporter] specs
+main = launchAff_ do
+  specs <- discover """^(?!(Milkis.Impl.Window|cache-db\.json|Web3\.Bindings|Web3|Web.XHR.XMLHttpRequestUpload|Web.XHR|Web.Storage|Web.Internal.FFI|Web|VoucherServer.Main)).*"""
+  runSpec [consoleReporter] specs
--- pkgs/purs/circles-pink-state-machine/test/Main.purs
+++ pkgs/purs/circles-pink-state-machine/test/Main.purs
@@ -25,7 +25,7 @@ mainTestUnit =
 
 mainTestSpec :: Effect Unit
 mainTestSpec = launchAff_ do
-  specs <- discover """.*"""
+  specs <- discover """^(?!(Milkis.Impl.Window|cache-db\.json|Web3\.Bindings|Web3|Web.XHR.XMLHttpRequestUpload|Web.XHR|Web.Storage|Web.Internal.FFI|Web|VoucherServer.Main)).*"""
   runSpec [consoleReporter] specs
 
 main :: Effect Unit
