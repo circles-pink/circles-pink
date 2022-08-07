47c732a2902d9581d0f8cd3810c1fd00b114a77d
Michael Bock <me@thought2.de>
2022-08-07 06:44:53 +0200

    Refactor AskEmail control

--- pkgs/purs/circles-pink-state-machine/src/CirclesPink/Garden/StateMachine/Control.purs
+++ pkgs/purs/circles-pink-state-machine/src/CirclesPink/Garden/StateMachine/Control.purs
@@ -8,6 +8,8 @@ import CirclesPink.Garden.StateMachine.Config (CirclesConfig)
 import CirclesPink.Garden.StateMachine.Control.Class (class MonadCircles)
 import CirclesPink.Garden.StateMachine.Control.EnvControl (EnvControl)
 import CirclesPink.Garden.StateMachine.Control.States as States
+import CirclesPink.Garden.StateMachine.Control.States.AskEmail as States.AskEmail
+
 import CirclesPink.Garden.StateMachine.State as S
 import Stadium.Control as C
 
@@ -25,7 +27,7 @@ circlesControl env cfg =
     _circlesStateMachine
     { landing: States.landing $ env
     , askUsername: States.askUsername env cfg
-    , askEmail: States.askEmail env cfg
+    , askEmail
     , infoSecurity: States.infoSecurity env
     , magicWords: States.magicWords env
     , submit: States.submit env
@@ -33,4 +35,18 @@ circlesControl env cfg =
     , login: States.login env
     , trusts: States.trusts env
     , debug: States.debug env
+    }
+  where
+
+  askEmail =
+    { prev:
+        States.AskEmail.prev env
+    , setTerms:
+        States.AskEmail.setTerms env
+    , setPrivacy:
+        States.AskEmail.setPrivacy env
+    , next:
+        States.AskEmail.next env
+    , setEmail:
+        States.AskEmail.setEmail env
     }
--- pkgs/purs/circles-pink-state-machine/src/CirclesPink/Garden/StateMachine/Control/States/AskEmail.purs
+++ pkgs/purs/circles-pink-state-machine/src/CirclesPink/Garden/StateMachine/Control/States/AskEmail.purs
@@ -2,59 +2,53 @@ module CirclesPink.Garden.StateMachine.Control.States.AskEmail where
 
 import Prelude
 
-import CirclesPink.Garden.StateMachine.Config (CirclesConfig)
+import CirclesPink.Garden.StateMachine.Control.Class (class MonadCircles)
 import CirclesPink.Garden.StateMachine.Control.Common (ActionHandler')
 import CirclesPink.Garden.StateMachine.Control.EnvControl (EnvControl)
 import CirclesPink.Garden.StateMachine.Direction as D
 import CirclesPink.Garden.StateMachine.State as S
 import Control.Monad.Except (runExceptT)
+import Control.Monad.Reader (ask)
 import Data.Either (Either(..))
 import Data.Newtype.Extra ((-#), ($-))
 import Data.Variant (default, onMatch)
 import RemoteData (_failure, _loading, _success)
 
-askEmail
-  :: forall m
-   . Monad m
-  => EnvControl m
-  -> CirclesConfig m
-  -> { prev :: ActionHandler' m Unit S.UserData ("askUsername" :: S.UserData)
-     , setEmail :: ActionHandler' m String S.UserData ("askEmail" :: S.UserData)
-     , setTerms :: ActionHandler' m Unit S.UserData ("askEmail" :: S.UserData)
-     , setPrivacy :: ActionHandler' m Unit S.UserData ("askEmail" :: S.UserData)
-     , next :: ActionHandler' m Unit S.UserData ("askEmail" :: S.UserData, "infoSecurity" :: S.UserData)
-     }
-askEmail env cfg =
-  { prev: \set _ _ -> set \st -> S._askUsername st { direction = D._backwards }
-  , setEmail
-  , setTerms: \set _ _ -> set \st -> S._askEmail st { terms = not st.terms }
-  , setPrivacy: \set _ _ -> set \st -> S._askEmail st { privacy = not st.privacy }
-  , next
-  }
-  where
-  next set st _ =
-    let
-      emailValid =
-        default false
-          # onMatch
-              { success: (\r -> r.isValid) }
-    in
-      if (emailValid $- st.emailApiResult) && st.terms && st.privacy then do
-        case cfg -# _.extractEmail of
-          Left _ -> pure unit
-          Right cb -> cb st.email
-        set \st' -> S._infoSecurity st' { direction = D._forwards }
-      else
-        set \st' -> S._askEmail st' { direction = D._forwards }
+prev :: forall m. MonadCircles m => EnvControl m -> ActionHandler' m Unit S.UserData ("askUsername" :: S.UserData)
+prev _ set _ _ = set \st -> S._askUsername st { direction = D._backwards }
 
-  setEmail set _ email = do
-    set \st -> S._askEmail st { email = email }
-    set \st -> S._askEmail st { emailApiResult = _loading unit }
-    result <- runExceptT $ env.apiCheckEmail email
-    set \st ->
-      if email == st.email then case result of
-        Left e -> S._askEmail st { emailApiResult = _failure e }
-        Right x -> S._askEmail st { emailApiResult = _success x }
-      else
-        S._askEmail st
-    pure unit
+setTerms :: forall m. MonadCircles m => EnvControl m -> ActionHandler' m Unit S.UserData ("askEmail" :: S.UserData)
+setTerms _ set _ _ = set \st -> S._askEmail st { terms = not st.terms }
+
+setPrivacy :: forall m. MonadCircles m => EnvControl m -> ActionHandler' m Unit S.UserData ("askEmail" :: S.UserData)
+setPrivacy _ set _ _ = set \st -> S._askEmail st { privacy = not st.privacy }
+
+next :: forall m. MonadCircles m => EnvControl m -> ActionHandler' m Unit S.UserData ("askEmail" :: S.UserData, "infoSecurity" :: S.UserData)
+next _ set st _ = do
+  cfg <- ask
+  let
+    emailValid =
+      default false
+        # onMatch
+            { success: (\r -> r.isValid) }
+
+  if (emailValid $- st.emailApiResult) && st.terms && st.privacy then do
+    case cfg -# _.extractEmail of
+      Left _ -> pure unit
+      Right cb -> cb st.email
+    set \st' -> S._infoSecurity st' { direction = D._forwards }
+  else
+    set \st' -> S._askEmail st' { direction = D._forwards }
+
+setEmail :: forall m. MonadCircles m => EnvControl m -> ActionHandler' m String S.UserData ("askEmail" :: S.UserData)
+setEmail env set _ email = do
+  set \st -> S._askEmail st { email = email }
+  set \st -> S._askEmail st { emailApiResult = _loading unit }
+  result <- runExceptT $ env.apiCheckEmail email
+  set \st ->
+    if email == st.email then case result of
+      Left e -> S._askEmail st { emailApiResult = _failure e }
+      Right x -> S._askEmail st { emailApiResult = _success x }
+    else
+      S._askEmail st
+  pure unit
