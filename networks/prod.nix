68ec9241ae630d47af1a2cd26814a346191f0d10
nmauersberg <n.mauersberg@gmail.com>
2022-08-16 13:46:55 +0200

    GAK 4 secrets

--- networks/prod.nix
+++ networks/prod.nix
@@ -36,7 +36,7 @@
           let
             json = builtins.fromJSON (builtins.readFile secretsFile);
           in
-          json
+          builtins.trace json json
         else mockSecrets;
 
     in
