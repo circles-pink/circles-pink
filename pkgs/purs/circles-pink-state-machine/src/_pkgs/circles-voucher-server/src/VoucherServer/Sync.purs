092351207340f652a75726df170a78a6b6d303db
Michael Bock <mb@thought2.de>
2022-08-18 12:44:37 +0200

    Fix todos

--- pkgs/purs/circles-pink-state-machine/src/_pkgs/circles-voucher-server/src/VoucherServer/Sync.purs
+++ pkgs/purs/circles-pink-state-machine/src/_pkgs/circles-voucher-server/src/VoucherServer/Sync.purs
@@ -2,7 +2,6 @@ module VoucherServer.Sync where
 
 import Prelude
 
-import CirclesPink.Garden.StateMachine (_debug)
 import Control.Monad.Error.Class (catchError, liftMaybe, throwError)
 import Control.Monad.Except (ExceptT(..), withExceptT)
 import Control.Monad.Reader (ask)
@@ -11,13 +10,12 @@ import Data.BN (BN)
 import Data.DateTime.Instant (Instant, unInstant)
 import Data.Maybe (Maybe)
 import Data.Newtype (un, unwrap)
-import Debug.Extra (todo)
 import Effect.Aff (Aff)
 import Payload.ResponseTypes (Response(..))
 import VoucherServer.MonadApp (class MonadApp, AppEnv(..), AppError(..), AppProdM, errorToLog, runAppProdM)
 import VoucherServer.MonadApp.Class (GraphNodeEnv(..))
 import VoucherServer.Spec.Types (EurCent(..), Freckles(..), VoucherAmount(..), VoucherEncrypted, VoucherOffer(..), VoucherProvider(..), VoucherProviderId(..))
-import VoucherServer.Specs.Xbge (Address(..))
+import VoucherServer.Specs.Xbge (Address)
 import VoucherServer.Types (Transfer(..), TransferMeta(..))
 
 finalizeTx' :: AppEnv AppProdM -> Transfer -> ExceptT String Aff VoucherEncrypted
