2cbd54f9b8a1665a697d3ff95c8b1f136cb9916f
nmauersberg <n.mauersberg@gmail.com>
2022-08-23 12:07:41 +0200

    Fix padding

--- pkgs/ts/@circles-pink/web-client/src/components/XbgeDialogCard.tsx
+++ pkgs/ts/@circles-pink/web-client/src/components/XbgeDialogCard.tsx
@@ -51,6 +51,6 @@ const Frame = styled.div((props: FrameProps) => [
 // UI / Layout Components
 // -----------------------------------------------------------------------------
 
-const CardBody = tw.div`max-w-7xl mx-auto sm:p-6 lg:p-8`;
+const CardBody = tw.div`max-w-7xl mx-auto p-4 sm:p-6 lg:p-8`;
 const MainContent = tw.div`w-full`;
 const Debug = tw.div`p-8`;
