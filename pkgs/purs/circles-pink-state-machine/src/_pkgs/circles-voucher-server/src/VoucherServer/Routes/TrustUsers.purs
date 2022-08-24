ed604bb6025e1238562890e6f9268a75409d0b42
Michael Bock <mb@thought2.de>
2022-08-24 16:52:18 +0200

    Fix syncing

--- pkgs/purs/circles-pink-state-machine/src/_pkgs/circles-voucher-server/src/VoucherServer/Routes/TrustUsers.purs
+++ pkgs/purs/circles-pink-state-machine/src/_pkgs/circles-voucher-server/src/VoucherServer/Routes/TrustUsers.purs
@@ -3,10 +3,11 @@ module VoucherServer.Routes.TrustUsers where
 import Prelude
 
 import CirclesPink.Data.Address (Address)
+import Control.Monad.Error.Class (catchError)
 import Control.Monad.Reader (ask)
 import Convertable (convert)
 import Data.Newtype (unwrap)
-import Data.Traversable (for)
+import Data.Traversable (for_)
 import VoucherServer.EnvVars (AppEnvVars(..))
 import VoucherServer.MonadApp (class MonadApp, AppEnv(..))
 import VoucherServer.MonadApp.Class (CirclesCoreEnv(..))
@@ -19,7 +20,8 @@ trustUsers
      }
   -> m {}
 trustUsers { body: { safeAddresses } } = do
-  _ <- for safeAddresses trustUser
+
+  for_ safeAddresses (\sa -> (void $ trustUser sa) `catchError` (\_ -> pure unit))
   pure {}
 
 trustUser :: forall m. MonadApp m => Address -> m String
