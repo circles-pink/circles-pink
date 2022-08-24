85b861f7e78661af2e1ffaebbdcf81188dcafc72
nmauersberg <n.mauersberg@gmail.com>
2022-08-25 00:43:48 +0200

    Shadowfriends in user search

--- pkgs/ts/@circles-pink/web-client/src/components/forms/index.tsx
+++ pkgs/ts/@circles-pink/web-client/src/components/forms/index.tsx
@@ -2,3 +2,4 @@ export { Button } from './Button';
 export { ButtonLinkLike } from './ButtonLinkLike';
 export { Checkbox } from './Checkbox';
 export { Input } from './Input';
+export { TagButton } from './TagButton';
