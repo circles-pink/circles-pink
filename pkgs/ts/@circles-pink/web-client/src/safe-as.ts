d87d9db3fdb2908a9a1465cc1552d024dd13eac8
Michael Bock <noday@posteo.net>
2022-09-13 17:36:21 +0200

    fromNativeBN

---
+++ pkgs/ts/@circles-pink/web-client/src/safe-as.ts
@@ -0,0 +1,5 @@
+import CirclesBN from '@circles-pink/state-machine/output/Data.BN';
+import BN from 'bn.js';
+
+export const toNativeBN = (x: CirclesBN.BN): BN => x as unknown as BN;
+export const fromNativeBN = (x: BN): CirclesBN.BN => x as unknown as CirclesBN.BN;
