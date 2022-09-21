0ef906e34e6be9f8935efb02b22e899ca6c46118
Michael Bock <no-day@posteo.net>
2022-09-21 09:50:41 +0200

    Refactor TrustNode

--- pkgs/purs/circles-pink-state-machine/src/_pkgs/circles-types/src/CirclesPink/Data/TrustNode.purs
+++ pkgs/purs/circles-pink-state-machine/src/_pkgs/circles-types/src/CirclesPink/Data/TrustNode.purs
@@ -1,4 +1,10 @@
-module CirclesPink.Data.TrustNode where
+module CirclesPink.Data.TrustNode
+  ( TrustNode(..)
+  , getAddress
+  , initTrustNode
+  , userIdent
+  )
+  where
 
 import CirclesPink.Prelude
 
@@ -6,43 +12,42 @@ import CirclesPink.Data.Address (Address)
 import CirclesPink.Data.User (User(..))
 import CirclesPink.Data.UserIdent (UserIdent(..))
 import Data.IxGraph (class Indexed)
-import Data.IxGraph as IxGraph 
+import Data.IxGraph as IxGraph
 import Data.Lens (Lens')
 import Data.Lens.Iso.Newtype (_Newtype)
 import Data.Lens.Record (prop)
-import Data.Newtype as NT
-import PursTsGen (class ToTsDef, class ToTsType)
-import PursTsGen.Lang.TypeScript.DSL as TS
+import PursTsGen.Lang.PureScript.Type as PS
 import Type.Proxy (Proxy(..))
 
-type TrustNode' = { userIdent :: UserIdent }
-
-newtype TrustNode = TrustNode TrustNode'
-
-derive instance Newtype TrustNode _
-derive newtype instance Show TrustNode
-derive newtype instance Eq TrustNode
-derive newtype instance Ord TrustNode
-
-instance ToTsDef TrustNode where
-  toTsDef _ = pure $ TS.typeDef (TS.name "TrustNode") []
-    $ TS.opaque (TS.qualName "CirclesPink_Data_TrustNode" "TrustNode") $ TS.name <$> []
-
-instance ToTsType TrustNode where
-  toTsType _ = TS.mkType_ $ TS.qualName "CirclesPink_Data_TrustNode" "TrustNode"
-
-unwrap :: TrustNode -> TrustNode'
-unwrap = NT.unwrap
-
-instance Indexed Address TrustNode where
-  getIndex (TrustNode { userIdent: UserIdent (Left x) }) = x
-  getIndex (TrustNode { userIdent: UserIdent (Right (User { safeAddress })) }) = safeAddress
+newtype TrustNode = TrustNode
+  { userIdent :: UserIdent
+  , isLoading :: Boolean
+  }
 
 getAddress :: TrustNode -> Address
 getAddress = IxGraph.getIndex
 
 initTrustNode :: UserIdent -> TrustNode
-initTrustNode userIdent' = TrustNode { userIdent: userIdent' }
+initTrustNode userIdent' = TrustNode
+  { userIdent: userIdent'
+  , isLoading: false
+  }
 
 userIdent :: Lens' TrustNode UserIdent
 userIdent = _Newtype <<< prop (Proxy :: _ "userIdent")
+
+derive newtype instance Show TrustNode
+derive newtype instance Eq TrustNode
+derive newtype instance Ord TrustNode
+
+derive instance Newtype TrustNode _
+
+instance Indexed Address TrustNode where
+  getIndex (TrustNode { userIdent: UserIdent (Left x) }) = x
+  getIndex (TrustNode { userIdent: UserIdent (Right (User { safeAddress })) }) = safeAddress
+
+instance ToTsType TrustNode where
+  toTsType = transprentRecordNewtype
+
+instance ToPursType TrustNode where
+  toPursType _ = PS.var $ PS.Name "TODO"
